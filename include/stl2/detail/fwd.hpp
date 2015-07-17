00f9ddbabb8f5ffb29aa5642e844a2709782ed37
Casey Carter <Casey@Carter.net>
2015-07-17 12:26:19 -0500

    Import forward/move/declval from std

--- include/stl2/detail/fwd.hpp
+++ include/stl2/detail/fwd.hpp
@@ -1,30 +1,13 @@
 #ifndef STL2_DETAIL_FWD_HPP
 #define STL2_DETAIL_FWD_HPP
 
-#include <type_traits>
+#include <utility>
 
 namespace stl2 { inline namespace v1 {
 
-template <class T>
-constexpr T&& forward(std::remove_reference_t<T>& t) noexcept {
-  return static_cast<T&&>(t);
-}
-
-template <class T>
-constexpr T&& forward(std::remove_reference_t<T>&& t) noexcept {
-  static_assert(!std::is_lvalue_reference<T>(),
-                "[forward]/3: \"If the second form is instantiated with "
-                "an lvalue reference type, the program is ill-formed.\"");
-  return static_cast<T&&>(t);
-}
-
-template <class T>
-constexpr std::remove_reference_t<T>&& move(T&& t) noexcept {
-  return static_cast<std::remove_reference_t<T>&&>(t);
-}
-
-template <class T>
-std::add_rvalue_reference_t<T> declval() noexcept;
+using std::forward;
+using std::move;
+using std::declval;
 
 }} // namespace stl2::v1
 
