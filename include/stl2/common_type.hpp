f3b882b30d79b21dadc022a86e2c0455b10a764b
Eric Niebler <eric@boostpro.com>
2015-07-24 15:34:17 -0700

    revert ill-advised change to common_reference

--- include/stl2/common_type.hpp
+++ include/stl2/common_type.hpp
@@ -130,12 +130,9 @@ struct common_reference<T> : meta::id<T> { };
 
 template <class T, class U>
 struct common_reference<T, U>
-  : common_type<T, U> { };
-
-template <class T, class U>
-  requires requires { typename __basic_common_reference<T, U>::type; }
-struct common_reference<T, U>
-  : __basic_common_reference<T, U> { };
+  : meta::if_<
+      meta::has_type<__basic_common_reference<T, U>>,
+      __basic_common_reference<T, U>, common_type<T, U>> { };
 
 template <class T, class U>
   requires requires {typename __builtin_common_t<T, U>;} &&
