edd4524deb21768bcda14298ae02da38be14f5ee
Casey Carter <Casey@Carter.net>
2015-08-02 17:30:52 -0500

    reverse_iterator

--- include/stl2/iterator.hpp
+++ include/stl2/iterator.hpp
@@ -792,6 +792,159 @@ I prev(I x, DifferenceType<I> n, S bound) {
 }
 
 ///////////////////////////////////////////////////////////////////////////
+// reverse_iterator [reverse.iterator]
+//
+template <BidirectionalIterator I>
+class reverse_iterator {
+public:
+  using iterator_type = I;
+  using difference_type = DifferenceType<I>;
+  using value_type = ValueType<I>;
+  using iterator_category = IteratorCategory<I>;
+  using reference = ReferenceType<I>;
+  using pointer = I;
+
+  reverse_iterator() : current{} {}
+  explicit reverse_iterator(I x) :
+    current{std::move(x)} {}
+
+  template <ConvertibleTo<I> U>
+  reverse_iterator(const reverse_iterator<U>& u) :
+    current{u.current} {}
+
+  template <ConvertibleTo<I> U>
+  reverse_iterator& operator=(const reverse_iterator<U>& u) & {
+    current = u.current;
+    return *this;
+  }
+
+  I base() const {
+    return current;
+  }
+  // 20150802: Extension
+  explicit operator const I&() const& {
+    return current;
+  }
+  // 20150802: Extension
+  explicit operator I&&() && {
+    return stl2::move(current);
+  }
+
+  reference operator*() const {
+    return *stl2::prev(current);
+  }
+
+  pointer operator->() const
+    requires _Is<I, std::is_pointer> ||
+      requires (const I& i) {
+        i.operator->();
+      }
+  {
+    return stl2::prev(current);
+  }
+
+  reverse_iterator& operator++() & {
+    --current;
+    return *this;
+  }
+  reverse_iterator operator++(int) & {
+    auto tmp = *this;
+    ++*this;
+    return tmp;
+  }
+
+  reverse_iterator& operator--() & {
+    ++current;
+    return *this;
+  }
+  reverse_iterator operator--(int) & {
+    auto tmp = *this;
+    --*this;
+    return tmp;
+  }
+
+  reverse_iterator operator+(difference_type n) const
+    requires RandomAccessIterator<I>() {
+    return reverse_iterator{current - n};
+  }
+  reverse_iterator& operator+=(difference_type n) &
+    requires RandomAccessIterator<I>() {
+    current -= n;
+    return *this;
+  }
+  reverse_iterator operator-(difference_type n) const
+    requires RandomAccessIterator<I>() {
+    return reverse_iterator{current + n};
+  }
+  reverse_iterator& operator-=(difference_type n) &
+    requires RandomAccessIterator<I>() {
+    current += n;
+    return *this;
+  }
+
+  decltype(auto) operator[](difference_type n) const
+    requires RandomAccessIterator<I>() {
+    return current[-n - 1];
+  }
+
+protected:
+  I current;
+};
+
+EqualityComparable{I1, I2}
+bool operator==(const reverse_iterator<I1>& x,
+                const reverse_iterator<I2>& y) {
+  return static_cast<const I1&>(x) == static_cast<const I2&>(y);
+}
+
+EqualityComparable{I1, I2}
+bool operator!=(const reverse_iterator<I1>& x,
+                const reverse_iterator<I2>& y) {
+  return static_cast<const I1&>(x) != static_cast<const I2&>(y);
+}
+
+TotallyOrdered{I1, I2}
+bool operator<(const reverse_iterator<I1>& x,
+               const reverse_iterator<I2>& y) {
+  return static_cast<const I1&>(x) > static_cast<const I2&>(y);
+}
+
+TotallyOrdered{I1, I2}
+bool operator>(const reverse_iterator<I1>& x,
+               const reverse_iterator<I2>& y) {
+  return static_cast<const I1&>(x) < static_cast<const I2&>(y);
+}
+
+TotallyOrdered{I1, I2}
+bool operator<=(const reverse_iterator<I1>& x,
+                const reverse_iterator<I2>& y) {
+  return static_cast<const I1&>(x) >= static_cast<const I2&>(y);
+}
+
+TotallyOrdered{I1, I2}
+bool operator>=(const reverse_iterator<I1>& x,
+                const reverse_iterator<I2>& y) {
+  return static_cast<const I1&>(x) <= static_cast<const I2&>(y);
+}
+
+SizedIteratorRange{I2, I1}
+DifferenceType<I2> operator-(const reverse_iterator<I1>& x,
+                             const reverse_iterator<I2>& y) {
+  return static_cast<const I2&>(y) - static_cast<const I1&>(x);
+}
+
+template <class I>
+reverse_iterator<I> operator+(DifferenceType<I> n,
+                              const reverse_iterator<I>& x) {
+  return reverse_iterator<I>{static_cast<const I&>(x) - n};
+}
+
+BidirectionalIterator{I}
+reverse_iterator<I> make_reverse_iterator(I i) {
+  return reverse_iterator<I>{std::move(i)};
+}
+
+///////////////////////////////////////////////////////////////////////////
 // iterator_traits [iterator.assoc]
 //
 template <WeakInputIterator I>
