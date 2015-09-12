2662fda27cacd860e6c4ad2c10ebb8a99aaa66b7
Casey Carter <Casey@Carter.net>
2015-09-11 23:29:46 -0500

    Implement find_if, steal test from range-v3

---
+++ include/stl2/detail/algorithm/find_if.hpp
@@ -0,0 +1,53 @@
+// cmcstl2 - A concept-enabled C++ standard library
+//
+//  Copyright Casey Carter 2015
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/caseycarter/cmcstl2
+//
+#ifndef STL2_DETAIL_ALGORITHM_FIND_IF_HPP
+#define STL2_DETAIL_ALGORITHM_FIND_IF_HPP
+
+#include <initializer_list>
+#include <stl2/functional.hpp>
+#include <stl2/iterator.hpp>
+#include <stl2/detail/fwd.hpp>
+#include <stl2/detail/concepts/callable.hpp>
+
+///////////////////////////////////////////////////////////////////////////
+// find_if [alg.find]
+//
+STL2_OPEN_NAMESPACE {
+  template <InputIterator I, Sentinel<I> S, class Proj = identity,
+            IndirectCallablePredicate<Projected<I, Proj>> Pred>
+  I find_if(I first, S last, Pred pred_, Proj proj_ = Proj{}) {
+    auto&& proj = __stl2::as_function(proj_);
+    auto&& pred = __stl2::as_function(pred_);
+    for (; first != last && !pred(proj(*first)); ++first) {
+      ;
+    }
+    return first;
+  }
+
+  template <InputRange Rng, class Proj = identity,
+            IndirectCallablePredicate<Projected<IteratorType<Rng>, Proj>> Pred>
+  safe_iterator_t<Rng>
+  find_if(Rng&& rng, Pred&& pred, Proj&& proj = Proj{}) {
+    return __stl2::find_if(__stl2::begin(rng), __stl2::end(rng),
+      __stl2::forward<Pred>(pred), __stl2::forward<Proj>(proj));
+  }
+
+  template <class E, class Proj = identity,
+            IndirectCallablePredicate<Projected<const E*, Proj>> Pred>
+  dangling<const E*>
+  find_if(std::initializer_list<E>&& il, Pred&& pred, Proj&& proj = Proj{}) {
+    return __stl2::find_if(il.begin(), il.end(),
+      __stl2::forward<Pred>(pred), __stl2::forward<Proj>(proj));
+  }
+} STL2_CLOSE_NAMESPACE
+
+#endif
---
+++ test/algorithm/find_if.cpp
@@ -0,0 +1,72 @@
+// Range v3 library
+//
+//  Copyright Eric Niebler 2014
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/ericniebler/range-v3
+
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include <stl2/detail/algorithm/find_if.hpp>
+#include <stl2/utility.hpp>
+#include "../simple_test.hpp"
+#include "../iterator/test_iterators.hpp"
+
+struct S
+{
+    int i_;
+};
+
+int main()
+{
+    using namespace __stl2;
+
+    int ia[] = {0, 1, 2, 3, 4, 5};
+    constexpr unsigned s = size(ia);
+    input_iterator<const int*> r = find_if(input_iterator<const int*>(ia),
+                                           input_iterator<const int*>(ia + s),
+                                           [](int i){return i == 3;});
+    CHECK(*r == 3);
+    r = find_if(input_iterator<const int*>(ia),
+                input_iterator<const int*>(ia+s),
+                [](int i){return i == 10;});
+    CHECK(r == input_iterator<const int*>(ia+s));
+
+    r = find_if(input_iterator<const int*>(ia),
+                sentinel<const int*>(ia+s),
+                [](int i){return i == 3;});
+    CHECK(*r == 3);
+    r = find_if(input_iterator<const int*>(ia),
+                sentinel<const int*>(ia+s),
+                [](int i){return i == 10;});
+    CHECK(r == input_iterator<const int*>(ia+s));
+
+    int *pi = find_if(ia, [](int i){return i == 3;});
+    CHECK(*pi == 3);
+    pi = find_if(ia, [](int i){return i == 10;});
+    CHECK(pi == ia+s);
+
+    auto pj = find_if(move(ia), [](int i){return i == 3;});
+    CHECK(*pj.get_unsafe() == 3);
+    pj = find_if(move(ia), [](int i){return i == 10;});
+    CHECK(pj.get_unsafe() == ia+s);
+
+    S sa[] = {{0}, {1}, {2}, {3}, {4}, {5}};
+    S *ps = find_if(sa, [](int i){return i == 3;}, &S::i_);
+    CHECK(ps->i_ == 3);
+    ps = find_if(sa, [](int i){return i == 10;}, &S::i_);
+    CHECK(ps == end(sa));
+
+    return ::test_result();
+}
