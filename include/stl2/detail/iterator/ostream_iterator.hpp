9d89b2cd8aa142ff7b5e549d8b9ffc32d5656204
Casey Carter <Casey@Carter.net>
2015-10-25 03:17:10 -0500

    Implement ostream_iterator<void>

--- include/stl2/detail/iterator/ostream_iterator.hpp
+++ include/stl2/detail/iterator/ostream_iterator.hpp
@@ -17,54 +17,104 @@
 #include <stl2/type_traits.hpp>
 #include <stl2/detail/fwd.hpp>
 #include <stl2/detail/raw_ptr.hpp>
+#include <stl2/detail/concepts/core.hpp>
 #include <stl2/detail/concepts/object.hpp>
 #include <stl2/detail/iostream/concepts.hpp>
 #include <stl2/detail/iterator/concepts.hpp>
 
 STL2_OPEN_NAMESPACE {
+  namespace detail {
+    ///////////////////////////////////////////////////////////////////////////
+    // ostream_iterator_base
+    // Common base class for ostream_iterator<T> and ostream_iterator<void>
+    //
+    template <class Derived, class charT, class traits>
+    class ostream_iterator_base {
+    public:
+      using iterator_category = output_iterator_tag;
+      using difference_type = std::ptrdiff_t;
+      using char_type = charT;
+      using traits_type = traits;
+      using ostream_type = std::basic_ostream<charT, traits>;
+
+      ostream_iterator_base() = default;
+
+      STL2_CONSTEXPR_EXT
+      ostream_iterator_base(ostream_type& s) noexcept :
+        stream_{&s} {}
+
+      STL2_CONSTEXPR_EXT
+      ostream_iterator_base(ostream_type& s, const charT* delimiter) noexcept :
+        stream_{&s}, delimiter_{delimiter} {}
+
+      STL2_CONSTEXPR_EXT
+      Derived& operator*() noexcept {
+        return static_cast<Derived&>(*this);
+      }
+
+      STL2_CONSTEXPR_EXT
+      Derived& operator++() & noexcept {
+        return static_cast<Derived&>(*this);
+      }
+
+      STL2_CONSTEXPR_EXT
+      Derived& operator++(int) & noexcept {
+        return static_cast<Derived&>(*this);
+      }
+
+    protected:
+      void delimit() {
+        if (delimiter_) {
+          *stream_ << delimiter_;
+        }
+      }
+
+      raw_ptr<ostream_type> stream_ = nullptr;
+      const char* delimiter_ = nullptr;
+    };
+  }
+
+  ///////////////////////////////////////////////////////////////////////////
+  // ostream_iterator [ostream.iterator]
   // Not to spec: StreamInsertable requirement is implicit.
-  template <class T, class charT = char,
+  //
+  template <class T = void, class charT = char,
     class traits = std::char_traits<charT>>
-      requires ext::StreamInsertable<T>
-  class ostream_iterator {
+    requires Same<T, void>() || ext::StreamInsertable<T>
+  class ostream_iterator :
+    public detail::ostream_iterator_base<
+      ostream_iterator<T, charT, traits>, charT, traits>
+  {
+    using base_t = typename ostream_iterator::ostream_iterator_base;
   public:
-    using iterator_category = output_iterator_tag;
-    using difference_type = std::ptrdiff_t;
-    using char_type = charT;
-    using traits_type = traits;
-    using ostream_type = std::basic_ostream<charT, traits>;
-
-    constexpr ostream_iterator() noexcept = default;
-
-    STL2_CONSTEXPR_EXT ostream_iterator(ostream_type& s) noexcept :
-      stream_{&s} {}
-
-    STL2_CONSTEXPR_EXT ostream_iterator(ostream_type& s, const charT* delimiter) noexcept :
-      stream_{&s}, delimiter_{delimiter} {}
+    ostream_iterator() = default;
+    using base_t::base_t;
 
     ostream_iterator& operator=(const T& t) & {
-      *stream_ << t;
-      if (delimiter_) {
-        *stream_ << delimiter_;
-      }
+      *base_t::stream_ << t;
+      base_t::delimit();
       return *this;
     }
+  };
 
-    STL2_CONSTEXPR_EXT ostream_iterator& operator*() noexcept {
+  ///////////////////////////////////////////////////////////////////////////
+  // ostream_iterator<void> [Extension]
+  //
+  template <class charT, class traits>
+  class ostream_iterator<void, charT, traits> :
+    public detail::ostream_iterator_base<
+      ostream_iterator<void, charT, traits>, charT, traits>
+  {
+    using base_t = typename ostream_iterator::ostream_iterator_base;
+  public:
+    ostream_iterator() = default;
+    using base_t::base_t;
+
+    ostream_iterator& operator=(const ext::StreamInsertable& t) & {
+      *base_t::stream_ << t;
+      base_t::delimit();
       return *this;
     }
-
-    STL2_CONSTEXPR_EXT ostream_iterator& operator++() & noexcept {
-      return *this;
-    }
-
-    STL2_CONSTEXPR_EXT ostream_iterator& operator++(int) & noexcept {
-      return *this;
-    }
-
-  private:
-    detail::raw_ptr<ostream_type> stream_ = nullptr;
-    const char* delimiter_ = nullptr;
   };
 } STL2_CLOSE_NAMESPACE
 
