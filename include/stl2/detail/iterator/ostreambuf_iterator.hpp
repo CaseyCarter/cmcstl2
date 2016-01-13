0592d2e7b491cf4404435dbb8441b5de7b9d6c43
Casey Carter <Casey@Carter.net>
2016-01-13 00:59:42 -0600

    [ostreambuf_iterator] Implement with basic_iterator

--- include/stl2/detail/iterator/ostreambuf_iterator.hpp
+++ include/stl2/detail/iterator/ostreambuf_iterator.hpp
@@ -18,95 +18,73 @@
 #include <stl2/detail/raw_ptr.hpp>
 #include <stl2/detail/concepts/fundamental.hpp>
 #include <stl2/detail/concepts/object.hpp>
+#include <stl2/detail/iterator/basic_iterator.hpp>
 #include <stl2/detail/iterator/concepts.hpp>
 #include <stl2/detail/iterator/default_sentinel.hpp>
 
 STL2_OPEN_NAMESPACE {
-  // Not to spec: MoveConstructible requirement is implicit
-  // Extension: models EqualityComparable and Sentinel<default_sentinel>
-  template <MoveConstructible charT, class traits = std::char_traits<charT>>
-  class ostreambuf_iterator {
-  public:
-    using iterator_category = output_iterator_tag;
-    using difference_type = std::ptrdiff_t;
+  namespace __ostreambuf_iterator {
+    template <MoveConstructible charT, class traits>
+    class cursor {
+      using ostream_type = std::basic_ostream<charT, traits>;
+      using streambuf_type = std::basic_streambuf<charT, traits>;
+      detail::raw_ptr<streambuf_type> sbuf_ = nullptr;
 
-    using char_type = charT;
-    using traits_type = traits;
-    using streambuf_type = std::basic_streambuf<charT, traits>;
-    using ostream_type = std::basic_ostream<charT, traits>;
+    public:
+      class mixin : protected detail::ebo_box<cursor> {
+        using box_t = detail::ebo_box<cursor>;
+      public:
+        using difference_type = __stl2::cursor::difference_type_t<cursor>;
+        using char_type = charT;
+        using traits_type = traits;
+        using ostream_type = cursor::ostream_type;
+        using streambuf_type = cursor::streambuf_type;
 
-    constexpr ostreambuf_iterator() noexcept = default;
-    STL2_CONSTEXPR_EXT ostreambuf_iterator(streambuf_type* s) noexcept
-    : sbuf_{s}
-    {}
-    ostreambuf_iterator(ostream_type& s) noexcept
-    : ostreambuf_iterator{s.rdbuf()}
-    {}
+        constexpr mixin() noexcept = default;
+        using box_t::box_t;
 
-    // Extension
-    constexpr ostreambuf_iterator(default_sentinel) noexcept :
-      ostreambuf_iterator{} {}
+        STL2_CONSTEXPR_EXT bool failed() const noexcept {
+          return box_t::get().sbuf_ == nullptr;
+        }
+      };
 
-    STL2_CONSTEXPR_EXT ostreambuf_iterator& operator*() {
-      return *this;
-    }
-    STL2_CONSTEXPR_EXT ostreambuf_iterator& operator++() & {
-      return *this;
-    }
-    STL2_CONSTEXPR_EXT ostreambuf_iterator operator++(int) & {
-      return *this;
-    }
+      constexpr cursor() noexcept = default;
+      STL2_CONSTEXPR_EXT cursor(streambuf_type* s) noexcept
+      : sbuf_{s}
+      {}
+      cursor(ostream_type& s) noexcept
+      : cursor{s.rdbuf()}
+      {}
+      // Extension
+      constexpr cursor(default_sentinel) noexcept
+      : cursor{}
+      {}
 
-    ostreambuf_iterator& operator=(charT c) & {
-      if (sbuf_) {
-        if (traits::eq_int_type(sbuf_->sputc(__stl2::move(c)), traits::eof())) {
-          sbuf_ = nullptr;
+      void write(charT c) {
+        if (sbuf_) {
+          if (traits::eq_int_type(sbuf_->sputc(__stl2::move(c)), traits::eof())) {
+            sbuf_ = nullptr;
+          }
         }
       }
-      return *this;
-    }
 
-    STL2_CONSTEXPR_EXT bool failed() const noexcept {
-      return sbuf_ == nullptr;
-    }
+      // Extension
+      bool equal(const cursor& that) const noexcept {
+        return sbuf_ == that.sbuf_;
+      }
 
-    // Extensions:
-    friend constexpr bool operator==(
-      const ostreambuf_iterator& lhs, const ostreambuf_iterator& rhs) noexcept
-    {
-      return lhs.sbuf_ == rhs.sbuf_;
-    }
-    friend constexpr bool operator!=(
-      const ostreambuf_iterator& lhs, const ostreambuf_iterator& rhs) noexcept
-    {
-      return !(lhs == rhs);
-    }
+      // Extension
+      bool equal(default_sentinel) const noexcept {
+        return sbuf_ == nullptr;
+      }
+    };
+  }
 
-    friend constexpr bool operator==(
-      const ostreambuf_iterator& lhs, default_sentinel) noexcept
-    {
-      return lhs.failed();
-    }
-    friend constexpr bool operator==(
-      default_sentinel lhs, const ostreambuf_iterator& rhs) noexcept
-    {
-      return rhs == lhs;
-    }
-
-    friend constexpr bool operator!=(
-      const ostreambuf_iterator& lhs, default_sentinel rhs) noexcept
-    {
-      return !(lhs == rhs);
-    }
-    friend constexpr bool operator!=(
-      default_sentinel lhs, const ostreambuf_iterator& rhs) noexcept
-    {
-      return !(rhs == lhs);
-    }
-
-  private:
-    detail::raw_ptr<streambuf_type> sbuf_ = nullptr;
-  };
+  // Not to spec: MoveConstructible requirement is implicit
+  // Extension: satisfies EqualityComparable and Sentinel<default_sentinel>
+  template <MoveConstructible charT, class traits = std::char_traits<charT>>
+  using ostreambuf_iterator =
+    basic_iterator<__ostreambuf_iterator::cursor<charT, traits>>;
 } STL2_CLOSE_NAMESPACE
 
 #endif
