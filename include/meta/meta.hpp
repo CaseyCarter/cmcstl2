024d495d2f7675a5806269312017293bad1a8523
Eric Niebler <eniebler@boost.org>
2016-03-25 21:17:31 -0700

    fix doxygen comment typo in meta.hpp

--- include/meta/meta.hpp
+++ include/meta/meta.hpp
@@ -566,8 +566,8 @@ namespace meta
         }
         /// \endcond
 
-        /// Test whether a type \c T is an instantiation of class
-        /// template \t C.
+        /// Test whether a type \p T is an instantiation of class
+        /// template \p C.
         /// \ingroup trait
         template <typename T, template <typename...> class C>
         using is = _t<detail::is_<T, C>>;
