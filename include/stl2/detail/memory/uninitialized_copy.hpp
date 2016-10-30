5f2e88a4b7194f8f91d52f24940570b8aeebff06
Christopher Di Bella <cjdb.ns@gmail.com>
2016-10-30 22:20:16 +1100

    Updated uninitialized_copy, etc. so that they resemble stl2/proposals-master/utilities.tex:uninitialized_copy, etc.

--- include/stl2/detail/memory/uninitialized_copy.hpp
+++ include/stl2/detail/memory/uninitialized_copy.hpp
@@ -17,23 +17,24 @@
 #include <stl2/detail/fwd.hpp>
 #include <stl2/detail/tagged.hpp>
 #include <stl2/detail/memory/destroy.hpp>
+#include <stl2/detail/memory/reference_to.hpp>
 
 STL2_OPEN_NAMESPACE {
    ///////////////////////////////////////////////////////////////////////////
    // uninitialized_copy [Extension]
    //
-   template <InputIterator I, Sentinel<I> S, WeaklyIncrementable O>
+   template <InputIterator I, Sentinel<I> S, /*NoThrow*/ForwardIterator O>
    requires
-      models::Constructible<value_type_t<O>, reference_t<I>> &&
-      models::Same<value_type_t<O>&, reference_t<O>> &&
-      __stl2::CopyConstructible<value_type_t<O>>()
+      Constructible<value_type_t<O>, reference_t<I>>() &&
+      __ReferenceTo<O, value_type_t<O>>()
    tagged_pair<tag::in(I), tag::out(O)>
    uninitialized_copy(I first, S last, O result)
    {
       auto saved = result;
       try {
-         for (; first != last; ++first, void(++result)) {
-            ::new(static_cast<void*>(__stl2::addressof(result))) value_type_t<I>(*first);
+         for (; first != last; ++result, (void)++first) {
+            ::new(const_cast<void*>(static_cast<const volatile void*>(__stl2::addressof(*result))))
+               value_type_t<I>(*first);
          }
       } catch(...) {
          for (; saved != result; ++saved)
@@ -43,33 +44,27 @@ STL2_OPEN_NAMESPACE {
       return {__stl2::move(first), __stl2::move(result)};
    }
 
-   template <InputRange Rng, class O>
+   template <InputRange Rng, /*NoThrow*/ForwardIterator O>
    requires
-      models::ForwardIterator<__f<O>> &&
-      models::Constructible<value_type_t<__f<O>>, reference_t<iterator_t<Rng>>> &&
-      models::Same<value_type_t<__f<O>>&, reference_t<__f<O>>>
-   auto //tagged_pair<tag::in(iterator_t<Rng>), tag::out(__f<O>)>
-   uninitialized_copy(Rng&& rng, O&& result)
+      Constructible<value_type_t<O>, reference_t<iterator_t<Rng>>>() &&
+      __ReferenceTo<O, value_type_t<O>>()
+   tagged_pair<tag::in(safe_iterator_t<Rng>), tag::out(O)>
+   uninitialized_copy(Rng&& rng, O result)
    {
-      return __stl2::uninitialized_copy(__stl2::begin(rng), __stl2::end(rng),
-                                                 __stl2::forward<O>(result));
+      return __stl2::uninitialized_copy(__stl2::begin(rng), __stl2::end(rng), result);
    }
 
    ///////////////////////////////////////////////////////////////////////////
    // uninitialized_copy_n [Extension]
    //
-   template <class I, class O>
+   template <InputIterator I, /*NoThrow*/ForwardIterator O>
    requires
-      models::InputIterator<__f<I>> &&
-      models::ForwardIterator<__f<O>> &&
-      models::Constructible<value_type_t<__f<O>>, reference_t<__f<I>>> &&
-      models::Same<value_type_t<__f<O>>&, reference_t<__f<O>>>
-   tagged_pair<tag::in(__f<I>), tag::out(__f<O>)>
-   uninitialized_copy_n(I&& first, difference_type_t<__f<I>> n, O&& out)
+      Constructible<value_type_t<O>, reference_t<I>>() &&
+      __ReferenceTo<O, value_type_t<O>>()
+   tagged_pair<tag::in(I), tag::out(O)>
+   uninitialized_copy_n(I first, difference_type_t<I> n, O out)
    {
-      auto counted = __stl2::make_counted_iterator(__stl2::forward<I>(first), n);
-      auto result = uninitialized_copy(__stl2::move(counted),
-         default_sentinel{}, __stl2::forward<O>(out));
+      auto result = uninitialized_copy(make_counted_iterator(first, n), default_sentinel{}, out);
       return {result.in().base(), result.out()};
    }
 } STL2_CLOSE_NAMESPACE
