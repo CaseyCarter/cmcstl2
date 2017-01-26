9cedf2bcf62c1d4af82ebcb54f15763a4fdb9704
Casey Carter <Casey@Carter.net>
2017-01-26 13:01:49 -0800

    indent any_iterator.hpp with tabs

--- include/stl2/detail/iterator/any_iterator.hpp
+++ include/stl2/detail/iterator/any_iterator.hpp
@@ -23,228 +23,228 @@
 
 STL2_OPEN_NAMESPACE {
 	namespace __any_iterator {
-        enum class op { copy, move, nuke, bump, comp, rval };
+		enum class op { copy, move, nuke, bump, comp, rval };
 
-        struct counted {
-            std::atomic<long> cnt{ 1 };
-        };
-        InputIterator{I} struct shared_iterator : counted {
-            shared_iterator(I i) : it(std::move(i)) {}
-            I it;
-        };
+		struct counted {
+			std::atomic<long> cnt{ 1 };
+		};
+		InputIterator{I} struct shared_iterator : counted {
+			shared_iterator(I i) : it(std::move(i)) {}
+			I it;
+		};
 
-        union blob {
-            counted* big;
-            std::aligned_storage_t<2 * sizeof(void *)> tiny;
-        };
+		union blob {
+			counted* big;
+			std::aligned_storage_t<2 * sizeof(void *)> tiny;
+		};
 
-        template <class It>
-        using is_small =
-            std::integral_constant<bool, (sizeof(It) <= sizeof(blob::tiny))>;
-        using small_tag = std::true_type;
-        using big_tag = std::false_type;
+		template <class It>
+		using is_small =
+			std::integral_constant<bool, (sizeof(It) <= sizeof(blob::tiny))>;
+		using small_tag = std::true_type;
+		using big_tag = std::false_type;
 
-        template <class RValueReference>
-        using iter_move_fn = RValueReference (*)(blob const &);
+		template <class RValueReference>
+		using iter_move_fn = RValueReference (*)(blob const &);
 
-        template <class RValueReference>
-        inline iter_move_fn<RValueReference> uninit_noop(op, blob *, blob *) {
-            return nullptr;
-        }
+		template <class RValueReference>
+		inline iter_move_fn<RValueReference> uninit_noop(op, blob *, blob *) {
+			return nullptr;
+		}
 
-        template <class Reference>
-        [[noreturn]] inline Reference uninit_deref(blob const &) {
-            std::terminate();
-        }
+		template <class Reference>
+		[[noreturn]] inline Reference uninit_deref(blob const &) {
+			std::terminate();
+		}
 
-        template <class Reference, InputIterator I>
-        Reference deref_small(blob const &src) {
-            return **static_cast<I const *>(static_cast<void const *>(&src.tiny));
-        }
+		template <class Reference, InputIterator I>
+		Reference deref_small(blob const &src) {
+			return **static_cast<I const *>(static_cast<void const *>(&src.tiny));
+		}
 
-        template <class Reference, InputIterator I>
-        Reference deref_big(blob const &src) {
-            return *static_cast<shared_iterator<I> const *>(src.big)->it;
-        }
+		template <class Reference, InputIterator I>
+		Reference deref_big(blob const &src) {
+			return *static_cast<shared_iterator<I> const *>(src.big)->it;
+		}
 
-        template <class I, class J>
-        constexpr bool iter_equal(I const &, J const &) {
-            return true;
-        }
+		template <class I, class J>
+		constexpr bool iter_equal(I const &, J const &) {
+			return true;
+		}
 
-        Sentinel{I, J}
-        constexpr bool iter_equal(I const &i, J const &j) {
-            return i == j;
-        }
+		Sentinel{I, J}
+		constexpr bool iter_equal(I const &i, J const &j) {
+			return i == j;
+		}
 
-        template <class RValueReference, InputIterator I>
-        iter_move_fn<RValueReference> exec_small(op o, blob *src, blob *dst) {
-            switch (o) {
-            case op::copy:
-                ::new (static_cast<void *>(&dst->tiny))
-                    I(*static_cast<I const *>(static_cast<void *>(&src->tiny)));
-                break;
-            case op::move:
-                ::new (static_cast<void *>(&dst->tiny))
-                    I(std::move(*static_cast<I *>(static_cast<void *>(&src->tiny))));
-                // fallthrough
-            case op::nuke:
-                static_cast<I *>(static_cast<void *>(&src->tiny))->~I();
-                break;
-            case op::bump:
-                ++*static_cast<I *>(static_cast<void *>(&src->tiny));
-                break;
-            case op::comp:
-                if (__any_iterator::iter_equal(
-                    *static_cast<I const *>(static_cast<void *>(&src->tiny)),
-                    *static_cast<I const *>(static_cast<void *>(&dst->tiny)))) {
-                    // fallthrough
-            case op::rval:
-                    return +[](blob const &src) -> RValueReference {
-                        return iter_move(
-                            *static_cast<I const *>(static_cast<void const *>(&src.tiny)));
-                    };
-                }
-            }
-            return nullptr;
-        }
+		template <class RValueReference, InputIterator I>
+		iter_move_fn<RValueReference> exec_small(op o, blob *src, blob *dst) {
+			switch (o) {
+			case op::copy:
+				::new (static_cast<void *>(&dst->tiny))
+					I(*static_cast<I const *>(static_cast<void *>(&src->tiny)));
+				break;
+			case op::move:
+				::new (static_cast<void *>(&dst->tiny))
+					I(std::move(*static_cast<I *>(static_cast<void *>(&src->tiny))));
+				// fallthrough
+			case op::nuke:
+				static_cast<I *>(static_cast<void *>(&src->tiny))->~I();
+				break;
+			case op::bump:
+				++*static_cast<I *>(static_cast<void *>(&src->tiny));
+				break;
+			case op::comp:
+				if (__any_iterator::iter_equal(
+					*static_cast<I const *>(static_cast<void *>(&src->tiny)),
+					*static_cast<I const *>(static_cast<void *>(&dst->tiny)))) {
+					// fallthrough
+			case op::rval:
+					return +[](blob const &src) -> RValueReference {
+						return iter_move(
+							*static_cast<I const *>(static_cast<void const *>(&src.tiny)));
+					};
+				}
+			}
+			return nullptr;
+		}
 
-        template <class RValueReference, InputIterator I>
-        iter_move_fn<RValueReference> exec_big(op o, blob *src, blob *dst) {
-            switch (o) {
-            case op::copy:
-                ++(dst->big = src->big)->cnt;
-               break;
-            case op::move:
-                dst->big = std::exchange(src->big, nullptr);
-                break;
-            case op::nuke:
-                if (0 == --src->big->cnt)
-                    delete static_cast<shared_iterator<I>*>(src->big);
-                break;
-            case op::bump:
-                ++static_cast<shared_iterator<I> *>(src->big)->it;
-                break;
-            case op::comp:
-                if (__any_iterator::iter_equal(
-                    static_cast<shared_iterator<I> const *>(src->big)->it,
-                    static_cast<shared_iterator<I> const *>(dst->big)->it)) {
-                    // fallthrough
-            case op::rval:
-                    return +[](blob const &src) -> RValueReference {
-                        return iter_move(
-                            static_cast<shared_iterator<I> const *>(src.big)->it);
-                    };
-                }
-            }
-            return nullptr;
-        }
+		template <class RValueReference, InputIterator I>
+		iter_move_fn<RValueReference> exec_big(op o, blob *src, blob *dst) {
+			switch (o) {
+			case op::copy:
+				++(dst->big = src->big)->cnt;
+				break;
+			case op::move:
+				dst->big = std::exchange(src->big, nullptr);
+				break;
+			case op::nuke:
+				if (0 == --src->big->cnt)
+					delete static_cast<shared_iterator<I>*>(src->big);
+				break;
+			case op::bump:
+				++static_cast<shared_iterator<I> *>(src->big)->it;
+				break;
+			case op::comp:
+				if (__any_iterator::iter_equal(
+					static_cast<shared_iterator<I> const *>(src->big)->it,
+					static_cast<shared_iterator<I> const *>(dst->big)->it)) {
+					// fallthrough
+			case op::rval:
+					return +[](blob const &src) -> RValueReference {
+						return iter_move(
+							static_cast<shared_iterator<I> const *>(src.big)->it);
+					};
+				}
+			}
+			return nullptr;
+		}
 
-        template <class Reference, class ValueType, class RValueReference>
-        struct cursor {
-        private:
-            blob data_ = { nullptr };
-            Reference (*deref_)(blob const &) =
-                &uninit_deref<Reference>;
-            iter_move_fn<RValueReference> (*exec_)(op, blob *, blob *) =
-                &uninit_noop<RValueReference>;
+		template <class Reference, class ValueType, class RValueReference>
+		struct cursor {
+		private:
+			blob data_ = { nullptr };
+			Reference (*deref_)(blob const &) =
+				&uninit_deref<Reference>;
+			iter_move_fn<RValueReference> (*exec_)(op, blob *, blob *) =
+				&uninit_noop<RValueReference>;
 
-            InputIterator{I} cursor(I i, small_tag) {
-                ::new (static_cast<void *>(&data_.tiny)) I(std::move(i));
-                deref_ = &deref_small<Reference, I>;
-                exec_ = &exec_small<RValueReference, I>;
-            }
-            InputIterator{I} cursor(I i, big_tag) {
-                data_.big = new shared_iterator<I>(std::move(i));
-                deref_ = &deref_big<Reference, I>;
-                exec_ = &exec_big<RValueReference, I>;
-            }
-            void reset() noexcept {
-                exec_(op::nuke, &data_, nullptr);
-                deref_ = &uninit_deref<Reference>;
-                exec_ = &uninit_noop<RValueReference>;
-            }
-            void copy_from(cursor const &that) {
-                // Pre: *this is empty
-                that.exec_(op::copy, const_cast<blob *>(&that.data_), &data_);
-                deref_ = that.deref_;
-                exec_ = that.exec_;
-            }
-            void move_from(cursor &that) {
-                // Pre: *this is empty
-                that.exec_(op::move, &that.data_, &data_);
-                swap(deref_, that.deref_);
-                swap(exec_, that.exec_);
-            }
-        public:
-            using value_type = ValueType;
-            using single_pass = std::true_type;
+			InputIterator{I} cursor(I i, small_tag) {
+				::new (static_cast<void *>(&data_.tiny)) I(std::move(i));
+				deref_ = &deref_small<Reference, I>;
+				exec_ = &exec_small<RValueReference, I>;
+			}
+			InputIterator{I} cursor(I i, big_tag) {
+				data_.big = new shared_iterator<I>(std::move(i));
+				deref_ = &deref_big<Reference, I>;
+				exec_ = &exec_big<RValueReference, I>;
+			}
+			void reset() noexcept {
+				exec_(op::nuke, &data_, nullptr);
+				deref_ = &uninit_deref<Reference>;
+				exec_ = &uninit_noop<RValueReference>;
+			}
+			void copy_from(cursor const &that) {
+				// Pre: *this is empty
+				that.exec_(op::copy, const_cast<blob *>(&that.data_), &data_);
+				deref_ = that.deref_;
+				exec_ = that.exec_;
+			}
+			void move_from(cursor &that) {
+				// Pre: *this is empty
+				that.exec_(op::move, &that.data_, &data_);
+				swap(deref_, that.deref_);
+				swap(exec_, that.exec_);
+			}
+		public:
+			using value_type = ValueType;
+			using single_pass = std::true_type;
 
-            struct mixin : basic_mixin<cursor> {
-            private:
-                using base_t = basic_mixin<cursor>;
-            public:
-                mixin() = default;
-                InputIterator{I} explicit mixin(I i)
-                : base_t(cursor{std::move(i)})
-                {}
+			struct mixin : basic_mixin<cursor> {
+			private:
+				using base_t = basic_mixin<cursor>;
+			public:
+				mixin() = default;
+				InputIterator{I} explicit mixin(I i)
+				: base_t(cursor{std::move(i)})
+				{}
 #if STL2_WORKAROUND_GCC_79143
-                explicit mixin(cursor c)
-                : base_t(std::move(c))
-                {}
+				explicit mixin(cursor c)
+				: base_t(std::move(c))
+				{}
 #else  // STL2_WORKAROUND_GCC_79143
-                using base_t::base_t;
+				using base_t::base_t;
 #endif // STL2_WORKAROUND_GCC_79143
-            };
+			};
 
-            cursor() = default;
-            cursor(cursor &&that) {
-                move_from(that);
-            }
-            cursor(cursor const &that) {
-                copy_from(that);
-            }
-            InputIterator{I} cursor(I i)
-            : cursor{std::move(i), is_small<I>{}}
-            {}
-            cursor &operator=(cursor &&that) {
-                if (&that != this) {
-                    reset();
-                    move_from(that);
-                }
-                return *this;
-            }
-            cursor &operator=(cursor const &that) {
-                if (&that != this) {
-                    reset();
-                    copy_from(that);
-                }
-                return *this;
-            }
-            ~cursor() {
-                exec_(op::nuke, &data_, nullptr);
-            }
-            Reference read() const {
-                return deref_(data_);
-            }
-            bool equal(cursor const &that) const {
-                return exec_(op::comp, const_cast<blob *>(&data_),
-                    const_cast<blob *>(&that.data_)) != nullptr;
-            }
-            void next() {
-                exec_(op::bump, &data_, nullptr);
-            }
-            RValueReference indirect_move() const {
-                return exec_(op::rval, nullptr, nullptr)(data_);
-            }
-        };
-    }
+			cursor() = default;
+			cursor(cursor &&that) {
+				move_from(that);
+			}
+			cursor(cursor const &that) {
+				copy_from(that);
+			}
+			InputIterator{I} cursor(I i)
+			: cursor{std::move(i), is_small<I>{}}
+			{}
+			cursor &operator=(cursor &&that) {
+				if (&that != this) {
+					reset();
+					move_from(that);
+				}
+				return *this;
+			}
+			cursor &operator=(cursor const &that) {
+				if (&that != this) {
+					reset();
+					copy_from(that);
+				}
+				return *this;
+			}
+			~cursor() {
+				exec_(op::nuke, &data_, nullptr);
+			}
+			Reference read() const {
+				return deref_(data_);
+			}
+			bool equal(cursor const &that) const {
+				return exec_(op::comp, const_cast<blob *>(&data_),
+					const_cast<blob *>(&that.data_)) != nullptr;
+			}
+			void next() {
+				exec_(op::bump, &data_, nullptr);
+			}
+			RValueReference indirect_move() const {
+				return exec_(op::rval, nullptr, nullptr)(data_);
+			}
+		};
+	}
 
-    template <class Reference,
-        class ValueType = __uncvref<Reference>,
-        class RValueReference = __iter_move::rvalue<Reference>>
-    using any_input_iterator =
-        basic_iterator<__any_iterator::cursor<Reference, ValueType, RValueReference>>;
+	template <class Reference,
+		class ValueType = __uncvref<Reference>,
+		class RValueReference = __iter_move::rvalue<Reference>>
+	using any_input_iterator =
+		basic_iterator<__any_iterator::cursor<Reference, ValueType, RValueReference>>;
 
 } STL2_CLOSE_NAMESPACE
 
