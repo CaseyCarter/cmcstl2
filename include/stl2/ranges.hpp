36015dfa2d24048b3f383c7a3a9efecb642cbcbf
Casey Carter <Casey@Carter.net>
2018-07-05 01:04:41 -0700

    Add headers <stl2/ranges.hpp> and <experimental/ranges/ranges>

---
+++ include/stl2/ranges.hpp
@@ -0,0 +1,42 @@
+// cmcstl2 - A concept-enabled C++ standard library
+//
+//  Copyright Casey Carter 2018
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/caseycarter/cmcstl2
+//
+#ifndef STL2_RANGES_HPP
+#define STL2_RANGES_HPP
+
+#include <initializer_list>
+#include <stl2/iterator.hpp>
+
+#include <stl2/detail/range/access.hpp>
+#include <stl2/detail/range/concepts.hpp>
+#include <stl2/detail/range/primitives.hpp>
+#include <stl2/view/all.hpp>
+#include <stl2/view/bounded.hpp>
+#include <stl2/view/counted.hpp>
+#include <stl2/view/empty.hpp>
+#include <stl2/view/filter.hpp>
+#include <stl2/view/indirect.hpp>
+#include <stl2/view/iota.hpp>
+#include <stl2/view/join.hpp>
+#include <stl2/view/move.hpp>
+#include <stl2/view/ref.hpp>
+#include <stl2/view/repeat_n.hpp>
+#include <stl2/view/repeat.hpp>
+#include <stl2/view/reverse.hpp>
+#include <stl2/view/single.hpp>
+#include <stl2/view/split.hpp>
+#include <stl2/view/subrange.hpp>
+#include <stl2/view/take.hpp>
+#include <stl2/view/take_exactly.hpp>
+#include <stl2/view/transform.hpp>
+#include <stl2/view/view_interface.hpp>
+
+#endif
