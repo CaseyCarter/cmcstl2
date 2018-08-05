7a6890d023864b1fb222932793876dd5b29236d4
Christopher Di Bella <cjdb.ns@gmail.com>
2018-08-05 13:39:54 +0100

    Added take_while_view as an extension to cmcslt2.

---
+++ include/stl2/view/take_while.hpp
@@ -0,0 +1,117 @@
+// cmcstl2 - A concept-enabled C++ standard library
+//
+//  Copyright Casey Carter
+//  Copyright Eric Niebler
+//  Copyright Christopher Di Bella
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/caseycarter/cmcstl2
+//
+#ifndef STL2_VIEW_TAKE_HPP
+#define STL2_VIEW_TAKE_HPP
+
+#include <stl2/detail/fwd.hpp>
+#include <stl2/detail/meta.hpp>
+#include <stl2/detail/concepts/callable.hpp>
+#include <stl2/detail/iterator/concepts.hpp>
+#include <stl2/detail/iterator/counted_iterator.hpp>
+#include <stl2/detail/semiregular_box.hpp>
+#include <stl2/detail/range/access.hpp>
+#include <stl2/detail/range/concepts.hpp>
+#include <stl2/detail/view/view_closure.hpp>
+#include <stl2/view/all.hpp>
+#include <stl2/view/view_interface.hpp>
+
+STL2_OPEN_NAMESPACE {
+	namespace ext {
+		template <View R, IndirectPredicate<iterator_t<R>> Pred>
+		class take_while_view
+		: public view_interface<take_while_view<R, Pred>>
+		, private detail::semiregular_box<Pred> {
+			template <bool> class __sentinel;
+			using storage_t = detail::semiregular_box<Pred>;
+			using storage_t::get;
+		public:
+			take_while_view() = default;
+
+			constexpr take_while_view(R base, Pred pred)
+			: storage_t{std::move(pred)}, base_(static_cast<R&&>(base)) {}
+
+			template <ViewableRange O>
+			requires _ConstructibleFromRange<R, O>
+			constexpr take_while_view(O&& o, Pred pred)
+			: storage_t{std::move(pred)}, base_(view::all(static_cast<O&&>(o))) {}
+
+			constexpr R base() const { return base_; }
+			constexpr const Pred& pred() const noexcept { return get(); }
+
+			constexpr auto begin() requires !SimpleView<R> { return begin_impl(*this); }
+			constexpr auto begin() const requires Range<const R> { return begin_impl(*this); }
+
+			constexpr auto end() requires !SimpleView<R> { return end_impl(*this); }
+			constexpr auto end() const requires Range<const R> || SizedRange<R>
+			{ return end_impl(*this); }
+		private:
+			R base_;
+
+			template <class Self>
+			static constexpr auto begin_impl(Self& self) { return __stl2::begin(self.base_); }
+
+			template <class Self>
+			static constexpr auto end_impl(Self& self) {
+				constexpr bool is_const = std::is_const_v<Self>;
+				return __sentinel<is_const>{__stl2::end(self.base_), &self.pred()};
+			}
+		};
+
+		template <Range R, IndirectPredicate<iterator_t<R>> Pred>
+		take_while_view(R&&, Pred) -> take_while_view<all_view<R>, Pred>;
+
+		template <class R, class Pred>
+		template <bool Const>
+		class take_while_view<R, Pred>::__sentinel {
+			using Base = __maybe_const<Const, R>;
+			sentinel_t<Base> end_ {};
+			const Pred* pred_;
+		public:
+			__sentinel() = default;
+
+			constexpr explicit __sentinel(sentinel_t<Base> end, const Pred* pred)
+			: end_(end), pred_(std::move(pred)) {}
+
+			constexpr __sentinel(__sentinel<!Const> s)
+			requires Const && ConvertibleTo<sentinel_t<R>, sentinel_t<Base>>
+			: end_(s.base()), pred_(std::move(s.pred_)) {}
+
+			constexpr sentinel_t<Base> base() const { return end_; }
+
+			friend constexpr bool operator==(const __sentinel& x, const iterator_t<Base> y)
+			{ return x.end_ == y || !(*x.pred_)(*y); }
+			friend constexpr bool operator==(const iterator_t<Base>& x, const __sentinel& y) { return y == x; }
+			friend constexpr bool operator!=(const __sentinel& x, const iterator_t<Base>& y) { return !(x == y); }
+			friend constexpr bool operator!=(const iterator_t<Base>& x, const __sentinel& y) { return !(y == x); }
+		};
+	} // namespace ext
+
+	namespace view::ext {
+		struct __take_while_fn : detail::__pipeable<__take_while_fn> {
+			template <class Rng, class Pred>
+			constexpr auto operator()(Rng&& rng, Pred&& pred) const
+			STL2_NOEXCEPT_REQUIRES_RETURN(
+				__stl2::ext::take_while_view{view::all(static_cast<Rng&&>(rng)), std::forward<Pred>(pred)}
+			)
+
+			template <CopyConstructible Pred>
+			constexpr auto operator()(Pred pred) const
+			{ return detail::view_closure{*this, std::move(pred)}; }
+		};
+
+		inline constexpr __take_while_fn take_while {};
+	} // namespace view::ext
+} STL2_CLOSE_NAMESPACE
+
+#endif
---
+++ test/view/take_while_view.cpp
@@ -0,0 +1,93 @@
+// cmcstl2 - A concept-enabled C++ standard library
+//
+//  Copyright Casey Carter
+//  Copyright Eric Niebler
+//  Copyright Christopher Di Bella
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/caseycarter/cmcstl2
+//
+#include <stl2/view/take_while.hpp>
+#include <stl2/view/iota.hpp>
+#include <list>
+#include <vector>
+#include "../simple_test.hpp"
+
+namespace ranges = __stl2;
+
+namespace view {
+	using namespace ranges::view;
+	using view::ext::take_while;
+} // namespace view
+
+int main()
+{
+	auto rng0 = view::iota(10) | view::take_while([](int i) { return i != 25; });
+	CHECK_EQUAL(rng0, {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24});
+	static_assert(ranges::View<decltype(rng0)>);
+	// ::models_not<concepts::BoundedView>(aux::copy(rng0));
+	static_assert(ranges::RandomAccessIterator<decltype(rng0.begin())>);
+
+	std::vector<int> vi{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	auto rng1 = vi | view::take_while([](int i) { return i != 50; });
+	static_assert(ranges::RandomAccessRange<decltype(rng1)>);
+	CHECK_EQUAL(rng1, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	//																															  //
+	// 						DISABLED because the lambda is not equality-preserving.						  //
+	//							Left in the review only to confirm that this is the case.					  //
+	//																															  //
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	// Check with a mutable predicate
+	// int rgi[] = {0,1,2,3,4,5,6,7,8,9};
+	// int cnt = 0;
+	// auto mutable_only = view::take_while(rgi, [cnt](int) mutable { return ++cnt <= 5;});
+	// CHECK_EQUAL(mutable_only, {0,1,2,3,4});
+	// static_assert(ranges::View<decltype(mutable_only)>);
+	// static_assert(!ranges::View<decltype(mutable_only) const>);
+
+	// Check with a mutable predicate
+	{
+		auto rng0 = view::iota(10) | view::take_while([](int i) mutable { return i != 25; });
+		CHECK_EQUAL(rng0, {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24});
+		static_assert(ranges::View<decltype(rng0)>);
+		static_assert(!ranges::View<decltype(rng0) const>);
+		// ::models_not<concepts::BoundedView>(aux::copy(rng0));
+		static_assert(ranges::RandomAccessIterator<decltype(rng0.begin())>);
+	}
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	//																															  //
+	// 								DISABLED until generate is migrated to cmcstl2.							  //
+	//																															  //
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	// {
+	// 	auto ns = view::generate([]() mutable {
+	// 		static int N;
+	// 		return ++N;
+	// 	});
+	// 	auto rng = ns | view::take_while([](int i) { return i < 5; });
+	// 	CHECK_EQUAL(rng, {1,2,3,4});
+	// }
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	//																															  //
+	// 								DISABLED until generate is migrated to cmcstl2.							  //
+	//																															  //
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	// {
+	// 	auto ns = view::generate([]() mutable {
+	// 		static int N;
+	// 		return ++N;
+	// 	});
+	// 	auto rng = ns | view::take_while([](int i) mutable { return i < 5; });
+	// 	CHECK_EQUAL(rng, {1,2,3,4});
+	// }
+
+	return ::test_result();
+}
