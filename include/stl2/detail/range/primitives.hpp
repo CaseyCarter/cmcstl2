9cb839e615b7ff065c64e254a75da13c8ca9960d
Christopher Di Bella <christopher@codeplay.com>
2018-10-31 14:37:29 +1100

    Updates next, prev, and distance to function objects to inhibit ADL.

--- include/stl2/detail/range/primitives.hpp
+++ include/stl2/detail/range/primitives.hpp
@@ -25,13 +25,50 @@
 STL2_OPEN_NAMESPACE {
 	// enumerate
 	namespace ext {
-		Range{R}
+		template<Iterator I, Sentinel<I> S>
+		constexpr tagged_pair<tag::count(iter_difference_t<I>), tag::end(I)>
+		enumerate(I first, S last)
+		noexcept(noexcept(++first != last) &&
+			std::is_nothrow_move_constructible<I>::value)
+		{
+			iter_difference_t<I> n = 0;
+			while (first != last) {
+				++n;
+				++first;
+			}
+			return {n, std::move(first)};
+		}
+
+		template<Iterator I, SizedSentinel<I> S>
+		constexpr tagged_pair<tag::count(iter_difference_t<I>), tag::end(I)>
+		enumerate(I first, S last)
+		noexcept(noexcept(__stl2::next(std::move(first), std::move(last))) &&
+			std::is_nothrow_move_constructible<I>::value)
+		{
+			auto d = last - first;
+			STL2_EXPECT((Same<I, S> || d >= 0));
+			return {d, __stl2::next(std::move(first), std::move(last))};
+		}
+
+		template<Iterator I, Sentinel<I> S>
+		requires (!SizedSentinel<S, I>) && SizedSentinel<I, I>
+		constexpr tagged_pair<tag::count(iter_difference_t<I>), tag::end(I)>
+		enumerate(I first, S last)
+		noexcept(noexcept(__stl2::next(first, std::move(last))) &&
+			std::is_nothrow_move_constructible<I>::value)
+		{
+			auto end = __stl2::next(first, std::move(last));
+			auto n = end - first;
+			return {n, std::move(end)};
+		}
+
+		template<Range R>
 		constexpr auto enumerate(R&& r)
 		STL2_NOEXCEPT_RETURN(
 			__stl2::ext::enumerate(__stl2::begin(r), __stl2::end(r))
 		)
 
-		SizedRange{R}
+		template<SizedRange R>
 		constexpr auto enumerate(R&& r)
 		STL2_NOEXCEPT_RETURN(
 			tagged_pair<tag::count(iter_difference_t<iterator_t<R>>),
@@ -42,25 +79,54 @@ STL2_OPEN_NAMESPACE {
 		)
 	}
 
-	Range{R}
-	constexpr iter_difference_t<iterator_t<R>> distance(R&& r)
-	noexcept(noexcept(__stl2::distance(__stl2::begin(r), __stl2::end(r))))
-	{
-		const iter_difference_t<iterator_t<R>> n =
-			__stl2::distance(__stl2::begin(r), __stl2::end(r));
-		STL2_EXPECT(n >= 0);
-		return n;
-	}
+	struct __distance_fn {
+		template<Iterator I, Sentinel<I> S>
+		constexpr iter_difference_t<I> operator()(I first, S last) const
+		// [[expects axiom: reachable(first, last)]]
+		STL2_NOEXCEPT_RETURN(
+			ext::enumerate(std::move(first), std::move(last)).first
+		)
 
-	SizedRange{R}
-	constexpr iter_difference_t<iterator_t<R>> distance(R&& r)
-	noexcept(noexcept(__stl2::size(r)))
-	{
-		const auto n =
-			static_cast<iter_difference_t<iterator_t<R>>>(__stl2::size(r));
-		STL2_EXPECT(n >= 0);
-		return n;
-	}
+		template<Iterator I, SizedSentinel<I> S>
+		constexpr iter_difference_t<I> operator()(I first, S last) const
+		noexcept(noexcept(last - first))
+		// [[expects axiom: reachable(first, last)]]
+		{
+			auto d = last - first;
+			STL2_EXPECT(d >= 0);
+			return d;
+		}
+
+		template<Iterator I>
+		requires SizedSentinel<I, I>
+		constexpr iter_difference_t<I> operator()(I first, I last) const
+		// [[expects axiom: reachable(first, last)]]
+		STL2_NOEXCEPT_RETURN(
+			last - first
+		)
+
+		template<Range R>
+		constexpr iter_difference_t<iterator_t<R>> operator()(R&& r) const
+		noexcept(noexcept(std::declval<__distance_fn const&>()(__stl2::begin(r), __stl2::end(r))))
+		{
+			const iter_difference_t<iterator_t<R>> n =
+				(*this)(__stl2::begin(r), __stl2::end(r));
+			STL2_EXPECT(n >= 0);
+			return n;
+		}
+
+		template<SizedRange R>
+		constexpr iter_difference_t<iterator_t<R>> operator()(R&& r) const
+		noexcept(noexcept(__stl2::size(r)))
+		{
+			const auto n =
+				static_cast<iter_difference_t<iterator_t<R>>>(__stl2::size(r));
+			STL2_EXPECT(n >= 0);
+			return n;
+		}
+	};
+
+	inline constexpr __distance_fn distance {};
 } STL2_CLOSE_NAMESPACE
 
 #endif
