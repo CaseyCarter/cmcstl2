08069a433e2d6d1faa65238c0021ff8d961ef329
Christopher Di Bella <cjdb.ns@gmail.com>
2018-12-11 21:47:12 +0000

    Synced ranges::partial_sort_copy with N4791

--- include/stl2/detail/algorithm/partial_sort_copy.hpp
+++ include/stl2/detail/algorithm/partial_sort_copy.hpp
@@ -28,57 +28,57 @@
 // partial_sort_copy [partial.sort.copy]
 //
 STL2_OPEN_NAMESPACE {
-	template<InputIterator I1, Sentinel<I1> S1,
-		RandomAccessIterator I2, Sentinel<I2> S2,
-		class Comp = less,
-		class Proj1 = identity, class Proj2 = identity>
-	requires
-		IndirectlyCopyable<I1, I2> &&
-		Sortable<I2, Comp, Proj2> &&
-		IndirectStrictWeakOrder<
-			Comp, projected<I1, Proj1>, projected<I2, Proj2>>
-	I2 partial_sort_copy(I1 first, S1 last, I2 result_first, S2 result_last,
-		Comp comp = {}, Proj1 proj1 = {}, Proj2 proj2 = {})
-	{
-		auto r = result_first;
-		if(r != result_last) {
-			auto cresult = ext::copy(std::move(first), last, std::move(r), result_last);
-			first = std::move(cresult.in);
-			r = std::move(cresult.out);
+	struct __partial_sort_copy_fn : private __niebloid {
+		template<InputIterator I1, Sentinel<I1> S1, RandomAccessIterator I2, Sentinel<I2> S2,
+			class Comp = less, class Proj1 = identity, class Proj2 = identity>
+		requires IndirectlyCopyable<I1, I2> &&
+			Sortable<I2, Comp, Proj2> &&
+			IndirectStrictWeakOrder<Comp, projected<I1, Proj1>, projected<I2, Proj2>>
+		constexpr I2
+		operator()(I1 first, S1 last, I2 result_first, S2 result_last,
+			Comp comp = {}, Proj1 proj1 = {}, Proj2 proj2 = {}) const
+		{
+			auto r = result_first;
+			if(r != result_last) {
+				auto cresult = ext::copy(std::move(first), last, std::move(r), result_last);
+				first = std::move(cresult.in);
+				r = std::move(cresult.out);
 
-			__stl2::make_heap(result_first, r, std::ref(comp), std::ref(proj2));
-			const auto len = __stl2::distance(result_first, r);
-			for(; first != last; ++first) {
-				iter_reference_t<I1>&& x = *first;
-				if(__stl2::invoke(comp, __stl2::invoke(proj1, x), __stl2::invoke(proj2, *result_first))) {
-					*result_first = std::forward<iter_reference_t<I1>>(x);
-					detail::sift_down_n(result_first, len, result_first,
-						std::ref(comp), std::ref(proj2));
+				__stl2::make_heap(result_first, r, std::ref(comp), std::ref(proj2));
+				const auto len = __stl2::distance(result_first, r);
+				for(; first != last; ++first) {
+					iter_reference_t<I1>&& x = *first;
+					if(__stl2::invoke(comp, __stl2::invoke(proj1, x), __stl2::invoke(proj2, *result_first))) {
+						*result_first = std::forward<iter_reference_t<I1>>(x);
+						detail::sift_down_n(result_first, len, result_first,
+							std::ref(comp), std::ref(proj2));
+					}
 				}
+				__stl2::sort_heap(result_first, r, std::ref(comp), std::ref(proj2));
 			}
-			__stl2::sort_heap(result_first, r, std::ref(comp), std::ref(proj2));
+			return r;
 		}
-		return r;
-	}
 
-	template<InputRange Rng1, RandomAccessRange Rng2, class Comp = less,
-		class Proj1 = identity, class Proj2 = identity>
-	requires
-		IndirectlyCopyable<iterator_t<Rng1>, iterator_t<Rng2>> &&
-		Sortable<iterator_t<Rng2>, Comp, Proj2> &&
-		IndirectStrictWeakOrder<Comp,
-			projected<iterator_t<Rng1>, Proj1>,
-			projected<iterator_t<Rng2>, Proj2>>
-	safe_iterator_t<Rng2>
-	partial_sort_copy(Rng1&& rng, Rng2&& result_rng, Comp comp = {},
-		Proj1 proj1 = {}, Proj2 proj2 = {})
-	{
-		return __stl2::partial_sort_copy(
-			begin(rng), end(rng),
-			begin(result_rng), end(result_rng),
-			std::ref(comp),
-			std::ref(proj1), std::ref(proj2));
-	}
+		template<InputRange R1, RandomAccessRange R2, class Comp = less,
+			class Proj1 = identity, class Proj2 = identity>
+		requires IndirectlyCopyable<iterator_t<R1>, iterator_t<R2>> &&
+			Sortable<iterator_t<R2>, Comp, Proj2> &&
+			IndirectStrictWeakOrder<Comp,
+				projected<iterator_t<R1>, Proj1>,
+				projected<iterator_t<R2>, Proj2>>
+		constexpr safe_iterator_t<R2>
+		operator()(R1&& r, R2&& result_r, Comp comp = {}, Proj1 proj1 = {}, Proj2 proj2 = {}) const
+		{
+			return (*this)(
+				begin(r), end(r),
+				begin(result_r), end(result_r),
+				std::ref(comp),
+				std::ref(proj1),
+				std::ref(proj2));
+		}
+	};
+
+	inline constexpr __partial_sort_copy_fn partial_sort_copy {};
 } STL2_CLOSE_NAMESPACE
 
 #endif
