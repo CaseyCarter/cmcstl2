e4ae5409bbfd25a63e3c1f1899b036c6af511520
Casey Carter <Casey@Carter.net>
2018-12-25 15:44:09 -0800

    unique_copy: sync to WD; niebloid; if constexpr

--- include/stl2/detail/algorithm/unique_copy.hpp
+++ include/stl2/detail/algorithm/unique_copy.hpp
@@ -1,6 +1,6 @@
 // cmcstl2 - A concept-enabled C++ standard library
 //
-//  Copyright Casey Carter 2015
+//  Copyright Casey Carter 2015-present
 //
 //  Use, modification and distribution is subject to the
 //  Boost Software License, Version 1.0. (See accompanying
@@ -23,109 +23,78 @@
 // unique_copy [alg.unique]
 //
 STL2_OPEN_NAMESPACE {
-	template<class I, class S, class O, class R, class Proj>
-	requires IndirectlyCopyableStorable<I, O>
-	tagged_pair<tag::in(I), tag::out(O)>
-	__unique_copy(ext::priority_tag<0>, I first, S last, O result, R comp, Proj proj)
-	{
-		if (first != last) {
-			iter_value_t<I> saved = *first;
-			*result = saved;
-			++result;
-			while (++first != last) {
-				iter_reference_t<I>&& v = *first;
-				if (!__stl2::invoke(comp,
-					__stl2::invoke(proj, std::forward<iter_reference_t<I>>(v)),
-					__stl2::invoke(proj, saved))) {
-					saved = std::forward<iter_reference_t<I>>(v);
+	template<class I, class O>
+	using unique_copy_result = __in_out_result<I, O>;
+
+	template<class I, class O>
+	META_CONCEPT __unique_copy_helper = InputIterator<I> &&  InputIterator<O> &&
+		Same<iter_value_t<I>, iter_value_t<O>>;
+
+	struct __unique_copy_fn : private __niebloid {
+		template<InputIterator I, Sentinel<I> S, WeaklyIncrementable O,
+			class Proj = identity, IndirectRelation<projected<I, Proj>> C = equal_to>
+		requires IndirectlyCopyable<I, O> &&
+			(ForwardIterator<I> || __unique_copy_helper<I, O> ||
+			 IndirectlyCopyableStorable<I, O>)
+		constexpr unique_copy_result<I, O>
+		operator()(I first, const S last, O result, C comp = {}, Proj proj = {}) const {
+			if (first != last) {
+				auto pred = [&comp, &proj](auto&& lhs, auto&& rhs) {
+					return !bool(__stl2::invoke(comp,
+						__stl2::invoke(proj, std::forward<decltype(lhs)>(lhs)),
+						__stl2::invoke(proj, std::forward<decltype(rhs)>(rhs))));
+				};
+				if constexpr (ForwardIterator<I>) {
+					*result = *first;
+					++result;
+					auto m = first;
+					while (++first != last) {
+						iter_reference_t<I>&& v = *first;
+						if (pred(std::forward<iter_reference_t<I>>(v), *m)) {
+							*result = std::forward<iter_reference_t<I>>(v);
+							++result;
+							m = first;
+						}
+					}
+				} else if constexpr (__unique_copy_helper<I, O>) {
+					*result = *first;
+					while (++first != last) {
+						iter_reference_t<I>&& v = *first;
+						if (pred(std::forward<iter_reference_t<I>>(v), *result)) {
+							*++result = std::forward<iter_reference_t<I>>(v);
+						}
+					}
+					++result;
+				} else { // IndirectlyCopyableStorable<I, O>
+					iter_value_t<I> saved = *first;
 					*result = saved;
 					++result;
+					while (++first != last) {
+						iter_reference_t<I>&& v = *first;
+						if (pred(std::forward<iter_reference_t<I>>(v), saved)) {
+							saved = std::forward<iter_reference_t<I>>(v);
+							*result = saved;
+							++result;
+						}
+					}
 				}
 			}
+			return {std::move(first), std::move(result)};
 		}
-		return {std::move(first), std::move(result)};
-	}
 
-	template<class I, class S, InputIterator O, class R, class Proj>
-	requires Same<iter_value_t<I>, iter_value_t<O>>
-	tagged_pair<tag::in(I), tag::out(O)>
-	__unique_copy(ext::priority_tag<1>, I first, S last, O result, R comp, Proj proj)
-	{
-		if (first != last) {
-			*result = *first;
-			while (++first != last) {
-				iter_reference_t<I>&& v = *first;
-				if (!__stl2::invoke(comp,
-					__stl2::invoke(proj, std::forward<iter_reference_t<I>>(v)),
-					__stl2::invoke(proj, *result))) {
-					*++result = std::forward<iter_reference_t<I>>(v);
-				}
-			}
-			++result;
+		template<InputRange R, WeaklyIncrementable O, class Proj = identity,
+			IndirectRelation<projected<iterator_t<R>, Proj>> C = equal_to>
+		requires IndirectlyCopyable<iterator_t<R>, O> &&
+			(ForwardRange<R> || __unique_copy_helper<iterator_t<R>, O> ||
+			 IndirectlyCopyableStorable<iterator_t<R>, O>)
+		constexpr unique_copy_result<safe_iterator_t<R>, O>
+		operator()(R&& r, O result, C comp = {}, Proj proj = {}) const {
+			return (*this)(begin(r), end(r), std::move(result),
+				__stl2::ref(comp), __stl2::ref(proj));
 		}
-		return {std::move(first), std::move(result)};
-	}
+	};
 
-	template<ForwardIterator I, class S, class O, class R, class Proj>
-	tagged_pair<tag::in(I), tag::out(O)>
-	__unique_copy(ext::priority_tag<2>, I first, S const last, O result, R comp, Proj proj)
-	{
-		if (first != last) {
-			*result = *first;
-			++result;
-			auto m = first;
-			while (++first != last) {
-				iter_reference_t<I>&& v = *first;
-				if (!__stl2::invoke(comp,
-					__stl2::invoke(proj, std::forward<iter_reference_t<I>>(v)),
-					__stl2::invoke(proj, *m))) {
-					*result = std::forward<iter_reference_t<I>>(v);
-					++result;
-					m = first;
-				}
-			}
-		}
-		return {std::move(first), std::move(result)};
-	}
-
-	template<InputIterator I, Sentinel<I> S, WeaklyIncrementable O,
-		class R = equal_to, class Proj = identity>
-	requires
-		IndirectlyCopyable<I, O> &&
-		IndirectRelation<R, projected<I, Proj>> &&
-		(ForwardIterator<I> ||
-		 (InputIterator<O> && Same<iter_value_t<I>, iter_value_t<O>>) ||
-		 IndirectlyCopyableStorable<I, O>)
-	tagged_pair<tag::in(I), tag::out(O)>
-	unique_copy(I first, S last, O result, R comp = {},
-		Proj proj = {})
-	{
-		return __stl2::__unique_copy(
-			ext::priority_tag<2>{},
-			std::move(first), std::move(last),
-			std::move(result),
-			__stl2::ref(comp),
-			__stl2::ref(proj));
-	}
-
-	template<InputRange Rng, WeaklyIncrementable O, class R = equal_to,
-		class Proj = identity>
-	requires
-		IndirectlyCopyable<iterator_t<Rng>, O> &&
-		IndirectRelation<R, projected<iterator_t<Rng>, Proj>> &&
-		(ForwardIterator<iterator_t<Rng>> ||
-		 (InputIterator<O> && Same<iter_value_t<iterator_t<Rng>>, iter_value_t<O>>) ||
-		 IndirectlyCopyableStorable<iterator_t<Rng>, O>)
-	tagged_pair<tag::in(safe_iterator_t<Rng>), tag::out(O)>
-	unique_copy(Rng&& rng, O result, R comp = {}, Proj proj = {})
-	{
-		return __stl2::__unique_copy(
-			ext::priority_tag<2>{},
-			begin(rng), end(rng),
-			std::move(result),
-			__stl2::ref(comp),
-			__stl2::ref(proj));
-	}
+	inline constexpr __unique_copy_fn unique_copy {};
 } STL2_CLOSE_NAMESPACE
 
 #endif
--- test/algorithm/unique_copy.cpp
+++ test/algorithm/unique_copy.cpp
@@ -33,107 +33,108 @@
 
 namespace ranges = __stl2;
 
-struct count_equal
-{
+struct count_equal {
 	static unsigned count;
+
 	template<class T>
-	bool operator()(const T& x, const T& y)
-		{++count; return x == y;}
+	bool operator()(const T& x, const T& y) {
+		++count;
+		return x == y;
+	}
 };
 
 unsigned count_equal::count = 0;
 
 template<class InIter, class OutIter, typename Sent = InIter>
-void
-test_iter()
-{
+void test_iter() {
 	const int ia[] = {0};
 	const unsigned sa = sizeof(ia)/sizeof(ia[0]);
 	int ja[sa] = {-1};
 	count_equal::count = 0;
-	std::pair<InIter, OutIter> r = ranges::unique_copy(InIter(ia), Sent(ia+sa), OutIter(ja), count_equal());
-	CHECK(base(r.first) == ia + sa);
-	CHECK(base(r.second) == ja + sa);
+	ranges::unique_copy_result<InIter, OutIter> r =
+		ranges::unique_copy(InIter(ia), Sent(ia+sa), OutIter(ja), count_equal());
+	CHECK(base(r.in) == ia + sa);
+	CHECK(base(r.out) == ja + sa);
 	CHECK(ja[0] == 0);
-	CHECK(count_equal::count == sa-1);
+	CHECK(count_equal::count == sa - 1);
 
 	const int ib[] = {0, 1};
 	const unsigned sb = sizeof(ib)/sizeof(ib[0]);
 	int jb[sb] = {-1};
 	count_equal::count = 0;
 	r = ranges::unique_copy(InIter(ib), Sent(ib+sb), OutIter(jb), count_equal());
-	CHECK(base(r.first) == ib + sb);
-	CHECK(base(r.second) == jb + sb);
+	CHECK(base(r.in) == ib + sb);
+	CHECK(base(r.out) == jb + sb);
 	CHECK(jb[0] == 0);
 	CHECK(jb[1] == 1);
-	CHECK(count_equal::count == sb-1);
+	CHECK(count_equal::count == sb - 1);
 
 	const int ic[] = {0, 0};
 	const unsigned sc = sizeof(ic)/sizeof(ic[0]);
 	int jc[sc] = {-1};
 	count_equal::count = 0;
 	r = ranges::unique_copy(InIter(ic), Sent(ic+sc), OutIter(jc), count_equal());
-	CHECK(base(r.first) == ic + sc);
-	CHECK(base(r.second) == jc + 1);
+	CHECK(base(r.in) == ic + sc);
+	CHECK(base(r.out) == jc + 1);
 	CHECK(jc[0] == 0);
-	CHECK(count_equal::count == sc-1);
+	CHECK(count_equal::count == sc - 1);
 
 	const int id[] = {0, 0, 1};
 	const unsigned sd = sizeof(id)/sizeof(id[0]);
 	int jd[sd] = {-1};
 	count_equal::count = 0;
 	r = ranges::unique_copy(InIter(id), Sent(id+sd), OutIter(jd), count_equal());
-	CHECK(base(r.first) == id + sd);
-	CHECK(base(r.second) == jd + 2);
+	CHECK(base(r.in) == id + sd);
+	CHECK(base(r.out) == jd + 2);
 	CHECK(jd[0] == 0);
 	CHECK(jd[1] == 1);
-	CHECK(count_equal::count == sd-1);
+	CHECK(count_equal::count == sd - 1);
 
 	const int ie[] = {0, 0, 1, 0};
 	const unsigned se = sizeof(ie)/sizeof(ie[0]);
 	int je[se] = {-1};
 	count_equal::count = 0;
 	r = ranges::unique_copy(InIter(ie), Sent(ie+se), OutIter(je), count_equal());
-	CHECK(base(r.first) == ie + se);
-	CHECK(base(r.second) == je + 3);
+	CHECK(base(r.in) == ie + se);
+	CHECK(base(r.out) == je + 3);
 	CHECK(je[0] == 0);
 	CHECK(je[1] == 1);
 	CHECK(je[2] == 0);
-	CHECK(count_equal::count == se-1);
+	CHECK(count_equal::count == se - 1);
 
 	const int ig[] = {0, 0, 1, 1};
 	const unsigned sg = sizeof(ig)/sizeof(ig[0]);
 	int jg[sg] = {-1};
 	count_equal::count = 0;
 	r = ranges::unique_copy(InIter(ig), Sent(ig+sg), OutIter(jg), count_equal());
-	CHECK(base(r.first) == ig + sg);
-	CHECK(base(r.second) == jg + 2);
+	CHECK(base(r.in) == ig + sg);
+	CHECK(base(r.out) == jg + 2);
 	CHECK(jg[0] == 0);
 	CHECK(jg[1] == 1);
-	CHECK(count_equal::count == sg-1);
+	CHECK(count_equal::count == sg - 1);
 
 	const int ih[] = {0, 1, 1};
 	const unsigned sh = sizeof(ih)/sizeof(ih[0]);
 	int jh[sh] = {-1};
 	count_equal::count = 0;
 	r = ranges::unique_copy(InIter(ih), Sent(ih+sh), OutIter(jh), count_equal());
-	CHECK(base(r.first) == ih + sh);
-	CHECK(base(r.second) == jh + 2);
+	CHECK(base(r.in) == ih + sh);
+	CHECK(base(r.out) == jh + 2);
 	CHECK(jh[0] == 0);
 	CHECK(jh[1] == 1);
-	CHECK(count_equal::count == sh-1);
+	CHECK(count_equal::count == sh - 1);
 
 	const int ii[] = {0, 1, 1, 1, 2, 2, 2};
 	const unsigned si = sizeof(ii)/sizeof(ii[0]);
 	int ji[si] = {-1};
 	count_equal::count = 0;
 	r = ranges::unique_copy(InIter(ii), Sent(ii+si), OutIter(ji), count_equal());
-	CHECK(base(r.first) == ii + si);
-	CHECK(base(r.second) == ji + 3);
+	CHECK(base(r.in) == ii + si);
+	CHECK(base(r.out) == ji + 3);
 	CHECK(ji[0] == 0);
 	CHECK(ji[1] == 1);
 	CHECK(ji[2] == 2);
-	CHECK(count_equal::count == si-1);
+	CHECK(count_equal::count == si - 1);
 }
 
 template<class InIter, class OutIter, typename Sent = InIter>
@@ -144,94 +145,94 @@ test_range()
 	const unsigned sa = sizeof(ia)/sizeof(ia[0]);
 	int ja[sa] = {-1};
 	count_equal::count = 0;
-	std::pair<InIter, OutIter> r = ranges::unique_copy(::as_lvalue(ranges::subrange(InIter(ia), Sent(ia+sa))), OutIter(ja), count_equal());
-	CHECK(base(r.first) == ia + sa);
-	CHECK(base(r.second) == ja + sa);
+	ranges::unique_copy_result<InIter, OutIter> r =
+		ranges::unique_copy(::as_lvalue(ranges::subrange(InIter(ia), Sent(ia+sa))), OutIter(ja), count_equal());
+	CHECK(base(r.in) == ia + sa);
+	CHECK(base(r.out) == ja + sa);
 	CHECK(ja[0] == 0);
-	CHECK(count_equal::count == sa-1);
+	CHECK(count_equal::count == sa - 1);
 
 	const int ib[] = {0, 1};
 	const unsigned sb = sizeof(ib)/sizeof(ib[0]);
 	int jb[sb] = {-1};
 	count_equal::count = 0;
 	r = ranges::unique_copy(::as_lvalue(ranges::subrange(InIter(ib), Sent(ib+sb))), OutIter(jb), count_equal());
-	CHECK(base(r.first) == ib + sb);
-	CHECK(base(r.second) == jb + sb);
+	CHECK(base(r.in) == ib + sb);
+	CHECK(base(r.out) == jb + sb);
 	CHECK(jb[0] == 0);
 	CHECK(jb[1] == 1);
-	CHECK(count_equal::count == sb-1);
+	CHECK(count_equal::count == sb - 1);
 
 	const int ic[] = {0, 0};
 	const unsigned sc = sizeof(ic)/sizeof(ic[0]);
 	int jc[sc] = {-1};
 	count_equal::count = 0;
 	r = ranges::unique_copy(::as_lvalue(ranges::subrange(InIter(ic), Sent(ic+sc))), OutIter(jc), count_equal());
-	CHECK(base(r.first) == ic + sc);
-	CHECK(base(r.second) == jc + 1);
+	CHECK(base(r.in) == ic + sc);
+	CHECK(base(r.out) == jc + 1);
 	CHECK(jc[0] == 0);
-	CHECK(count_equal::count == sc-1);
+	CHECK(count_equal::count == sc - 1);
 
 	const int id[] = {0, 0, 1};
 	const unsigned sd = sizeof(id)/sizeof(id[0]);
 	int jd[sd] = {-1};
 	count_equal::count = 0;
 	r = ranges::unique_copy(::as_lvalue(ranges::subrange(InIter(id), Sent(id+sd))), OutIter(jd), count_equal());
-	CHECK(base(r.first) == id + sd);
-	CHECK(base(r.second) == jd + 2);
+	CHECK(base(r.in) == id + sd);
+	CHECK(base(r.out) == jd + 2);
 	CHECK(jd[0] == 0);
 	CHECK(jd[1] == 1);
-	CHECK(count_equal::count == sd-1);
+	CHECK(count_equal::count == sd - 1);
 
 	const int ie[] = {0, 0, 1, 0};
 	const unsigned se = sizeof(ie)/sizeof(ie[0]);
 	int je[se] = {-1};
 	count_equal::count = 0;
 	r = ranges::unique_copy(::as_lvalue(ranges::subrange(InIter(ie), Sent(ie+se))), OutIter(je), count_equal());
-	CHECK(base(r.first) == ie + se);
-	CHECK(base(r.second) == je + 3);
+	CHECK(base(r.in) == ie + se);
+	CHECK(base(r.out) == je + 3);
 	CHECK(je[0] == 0);
 	CHECK(je[1] == 1);
 	CHECK(je[2] == 0);
-	CHECK(count_equal::count == se-1);
+	CHECK(count_equal::count == se - 1);
 
 	const int ig[] = {0, 0, 1, 1};
 	const unsigned sg = sizeof(ig)/sizeof(ig[0]);
 	int jg[sg] = {-1};
 	count_equal::count = 0;
 	r = ranges::unique_copy(::as_lvalue(ranges::subrange(InIter(ig), Sent(ig+sg))), OutIter(jg), count_equal());
-	CHECK(base(r.first) == ig + sg);
-	CHECK(base(r.second) == jg + 2);
+	CHECK(base(r.in) == ig + sg);
+	CHECK(base(r.out) == jg + 2);
 	CHECK(jg[0] == 0);
 	CHECK(jg[1] == 1);
-	CHECK(count_equal::count == sg-1);
+	CHECK(count_equal::count == sg - 1);
 
 	const int ih[] = {0, 1, 1};
 	const unsigned sh = sizeof(ih)/sizeof(ih[0]);
 	int jh[sh] = {-1};
 	count_equal::count = 0;
 	r = ranges::unique_copy(::as_lvalue(ranges::subrange(InIter(ih), Sent(ih+sh))), OutIter(jh), count_equal());
-	CHECK(base(r.first) == ih + sh);
-	CHECK(base(r.second) == jh + 2);
+	CHECK(base(r.in) == ih + sh);
+	CHECK(base(r.out) == jh + 2);
 	CHECK(jh[0] == 0);
 	CHECK(jh[1] == 1);
-	CHECK(count_equal::count == sh-1);
+	CHECK(count_equal::count == sh - 1);
 
 	const int ii[] = {0, 1, 1, 1, 2, 2, 2};
 	const unsigned si = sizeof(ii)/sizeof(ii[0]);
 	int ji[si] = {-1};
 	count_equal::count = 0;
 	r = ranges::unique_copy(::as_lvalue(ranges::subrange(InIter(ii), Sent(ii+si))), OutIter(ji), count_equal());
-	CHECK(base(r.first) == ii + si);
-	CHECK(base(r.second) == ji + 3);
+	CHECK(base(r.in) == ii + si);
+	CHECK(base(r.out) == ji + 3);
 	CHECK(ji[0] == 0);
 	CHECK(ji[1] == 1);
 	CHECK(ji[2] == 2);
-	CHECK(count_equal::count == si-1);
+	CHECK(count_equal::count == si - 1);
 }
 
 template<class InIter, class OutIter>
-void test()
-{
+void test() {
 	using Sent = typename sentinel_type<InIter>::type;
 	test_iter<InIter, OutIter>();
 	test_iter<InIter, OutIter, Sent>();
@@ -240,23 +241,19 @@ void test()
 	test_range<InIter, OutIter, Sent>();
 }
 
-struct S
-{
-	int i,j;
+struct S {
+	int i, j;
 };
 
-bool operator==(S l, S r)
-{
+bool operator==(S l, S r) {
 	return l.i == r.i && l.j == r.j;
 }
 
-bool operator!=(S l, S r)
-{
+bool operator!=(S l, S r) {
 	return !(l == r);
 }
 
-int main()
-{
+int main() {
 	test<input_iterator<const int*>, output_iterator<int*> >();
 	test<input_iterator<const int*>, forward_iterator<int*> >();
 	test<input_iterator<const int*>, bidirectional_iterator<int*> >();
@@ -291,9 +288,10 @@ int main()
 	{
 		S const ia[] = {{1,1},{2,2},{3,3},{3,4},{4,5},{5,6},{5,7},{5,8},{6,9},{7,10}};
 		S ib[ranges::size(ia)];
-		std::pair<S const *, S *> r = ranges::unique_copy(ia, ib, ranges::equal_to(), &S::i);
-		CHECK(r.first == ranges::end(ia));
-		CHECK(r.second == ib + 7);
+		ranges::unique_copy_result<const S*, S*> r =
+			ranges::unique_copy(ia, ib, ranges::equal_to(), &S::i);
+		CHECK(r.in == ranges::end(ia));
+		CHECK(r.out == ib + 7);
 		CHECK_EQUAL(ranges::subrange(ib, ib+7), {S{1,1},S{2,2},S{3,3},S{4,5},S{5,6},S{6,9},S{7,10}});
 	}
 
@@ -302,8 +300,8 @@ int main()
 		S const ia[] = {{1,1},{2,2},{3,3},{3,4},{4,5},{5,6},{5,7},{5,8},{6,9},{7,10}};
 		S ib[ranges::size(ia)];
 		auto r = ranges::unique_copy(std::move(ia), ib, ranges::equal_to(), &S::i);
-		static_assert(ranges::Same<decltype(r.first), ranges::dangling>);
-		CHECK(r.second == ib + 7);
+		static_assert(ranges::Same<decltype(r.in), ranges::dangling>);
+		CHECK(r.out == ib + 7);
 		CHECK_EQUAL(ranges::subrange(ib, ib+7), {S{1,1},S{2,2},S{3,3},S{4,5},S{5,6},S{6,9},S{7,10}});
 	}
 
