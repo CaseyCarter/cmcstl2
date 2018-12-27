99222ce2437020ae930b2057f98b4243bde887a5
Casey Carter <Casey@Carter.net>
2018-12-27 14:15:46 -0800

    min,max,minmax: publics-before-privates

--- include/stl2/detail/algorithm/min.hpp
+++ include/stl2/detail/algorithm/min.hpp
@@ -13,36 +13,15 @@
 #define STL2_DETAIL_ALGORITHM_MIN_HPP
 
 #include <initializer_list>
-#include <stl2/functional.hpp>
-#include <stl2/iterator.hpp>
-#include <stl2/detail/fwd.hpp>
+
 #include <stl2/detail/concepts/callable.hpp>
-#include <stl2/detail/concepts/object.hpp>
+#include <stl2/detail/range/primitives.hpp>
 
 ///////////////////////////////////////////////////////////////////////////
 // min [alg.min.max]
 //
 STL2_OPEN_NAMESPACE {
 	struct __min_fn : private __niebloid {
-	private:
-		template<InputRange R, class Proj = identity,
-			IndirectStrictWeakOrder<projected<iterator_t<R>, Proj>> Comp = less>
-		requires IndirectlyCopyableStorable<iterator_t<R>, iter_value_t<iterator_t<R>>*>
-		static constexpr iter_value_t<iterator_t<R>>
-		impl(R&& r, Comp comp = {}, Proj proj = {}) {
-			auto first = begin(r);
-			auto last = end(r);
-			STL2_EXPECT(first != last);
-			iter_value_t<iterator_t<R>> result = *first;
-			while (++first != last) {
-				auto&& tmp = *first;
-				if (__stl2::invoke(comp, __stl2::invoke(proj, tmp), __stl2::invoke(proj, result))) {
-					result = static_cast<decltype(tmp)>(tmp);
-				}
-			}
-			return result;
-		}
-	public:
 		template<class T, class Proj = identity,
 			IndirectStrictWeakOrder<projected<const T*, Proj>> Comp = less>
 		constexpr const T&
@@ -65,6 +44,27 @@ STL2_OPEN_NAMESPACE {
 		constexpr iter_value_t<iterator_t<R>>
 		operator()(R&& r, Comp comp = {}, Proj proj = {}) const {
 			return impl(r, __stl2::ref(comp), __stl2::ref(proj));
+		}
+	private:
+		template<InputRange R, class Proj = identity,
+			IndirectStrictWeakOrder<projected<iterator_t<R>, Proj>> Comp = less>
+		requires
+			IndirectlyCopyableStorable<iterator_t<R>, iter_value_t<iterator_t<R>>*>
+		static constexpr iter_value_t<iterator_t<R>>
+		impl(R&& r, Comp comp = {}, Proj proj = {}) {
+			auto first = begin(r);
+			auto last = end(r);
+			STL2_EXPECT(first != last);
+			iter_value_t<iterator_t<R>> result = *first;
+			while (++first != last) {
+				auto&& tmp = *first;
+				if (__stl2::invoke(comp,
+						__stl2::invoke(proj, tmp),
+						__stl2::invoke(proj, result))) {
+					result = static_cast<decltype(tmp)>(tmp);
+				}
+			}
+			return result;
 		}
 	};
 
