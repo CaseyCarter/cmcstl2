499240e7febf81e56f81cd36e3d69aecb201efde
Casey Carter <Casey@Carter.net>
2018-12-27 16:37:16 -0800

    remove,remove_if: niebloid

--- include/stl2/detail/algorithm/remove_copy.hpp
+++ include/stl2/detail/algorithm/remove_copy.hpp
@@ -12,10 +12,9 @@
 #ifndef STL2_DETAIL_ALGORITHM_REMOVE_COPY_HPP
 #define STL2_DETAIL_ALGORITHM_REMOVE_COPY_HPP
 
-#include <stl2/functional.hpp>
-#include <stl2/iterator.hpp>
 #include <stl2/detail/algorithm/results.hpp>
 #include <stl2/detail/concepts/callable.hpp>
+#include <stl2/detail/range/primitives.hpp>
 
 ///////////////////////////////////////////////////////////////////////////
 // remove_copy [alg.remove]
--- include/stl2/detail/algorithm/remove_if.hpp
+++ include/stl2/detail/algorithm/remove_if.hpp
@@ -12,47 +12,43 @@
 #ifndef STL2_DETAIL_ALGORITHM_REMOVE_IF_HPP
 #define STL2_DETAIL_ALGORITHM_REMOVE_IF_HPP
 
-#include <stl2/iterator.hpp>
-#include <stl2/detail/fwd.hpp>
 #include <stl2/detail/algorithm/find_if.hpp>
 #include <stl2/detail/concepts/callable.hpp>
+#include <stl2/detail/range/primitives.hpp>
 
 ///////////////////////////////////////////////////////////////////////////
-// remove [alg.remove]
+// remove_if [alg.remove]
 //
 STL2_OPEN_NAMESPACE {
-	template<ForwardIterator I, Sentinel<I> S, class Pred, class Proj = identity>
-	requires
-		Permutable<I> &&
-		IndirectUnaryPredicate<
-			Pred, projected<I, Proj>>
-	I remove_if(I first, S last, Pred pred, Proj proj = {})
-	{
-		first = find_if(std::move(first), last,
-			__stl2::ref(pred), __stl2::ref(proj));
-		if (first != last) {
-			for (auto m = next(first); m != last; ++m) {
-				if (!__stl2::invoke(pred, __stl2::invoke(proj, *m))) {
-					*first = iter_move(m);
-					++first;
+	struct __remove_if_fn : private __niebloid {
+		template<Permutable I, Sentinel<I> S, class Proj = identity,
+			IndirectUnaryPredicate<projected<I, Proj>> Pred>
+		constexpr I
+		operator()(I first, S last, Pred pred, Proj proj = {}) const {
+			first = find_if(std::move(first), last, __stl2::ref(pred),
+				__stl2::ref(proj));
+			if (first != last) {
+				for (auto m = next(first); m != last; ++m) {
+					if (!__stl2::invoke(pred, __stl2::invoke(proj, *m))) {
+						*first = iter_move(m);
+						++first;
+					}
 				}
 			}
+			return first;
 		}
-		return first;
-	}
 
-	template<ForwardRange Rng, class Pred, class Proj = identity>
-	requires
-		Permutable<iterator_t<Rng>> &&
-		IndirectUnaryPredicate<
-			Pred, projected<iterator_t<Rng>, Proj>>
-	safe_iterator_t<Rng>
-	remove_if(Rng&& rng, Pred pred, Proj proj = {})
-	{
-		return __stl2::remove_if(
-			begin(rng), end(rng),
-			__stl2::ref(pred), __stl2::ref(proj));
-	}
+		template<ForwardRange Rng, class Proj = identity,
+			IndirectUnaryPredicate<projected<iterator_t<Rng>, Proj>> Pred>
+		requires Permutable<iterator_t<Rng>>
+		constexpr safe_iterator_t<Rng>
+		operator()(Rng&& rng, Pred pred, Proj proj = {}) const {
+			return (*this)(begin(rng), end(rng), __stl2::ref(pred),
+				__stl2::ref(proj));
+		}
+	};
+
+	inline constexpr __remove_if_fn remove_if {};
 } STL2_CLOSE_NAMESPACE
 
 #endif
