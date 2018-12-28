96bf66ee143d4e979b760b9e4981bd65990b6d9b
Casey Carter <Casey@Carter.net>
2018-12-27 21:01:43 -0800

    unique: niebloid

--- include/stl2/detail/algorithm/unique.hpp
+++ include/stl2/detail/algorithm/unique.hpp
@@ -12,46 +12,45 @@
 #ifndef STL2_DETAIL_ALGORITHM_UNIQUE_HPP
 #define STL2_DETAIL_ALGORITHM_UNIQUE_HPP
 
-#include <stl2/functional.hpp>
-#include <stl2/iterator.hpp>
-#include <stl2/detail/fwd.hpp>
 #include <stl2/detail/algorithm/adjacent_find.hpp>
 #include <stl2/detail/concepts/callable.hpp>
+#include <stl2/detail/range/primitives.hpp>
 
 ///////////////////////////////////////////////////////////////////////////
 // unique [alg.unique]
 //
 STL2_OPEN_NAMESPACE {
-	template<ForwardIterator I, Sentinel<I> S,
-		class R = equal_to, class Proj = identity>
-	requires
-		Permutable<I> &&
-		IndirectRelation<__f<R>, projected<I, Proj>>
-	I unique(I first, S last, R comp = {}, Proj proj = {})
-	{
-		first = adjacent_find(std::move(first), last, __stl2::ref(comp), __stl2::ref(proj));
-		if (first != last) {
-			for (auto m = next(first, 2); m != last; ++m) {
-				if (!__stl2::invoke(comp, __stl2::invoke(proj, *first), __stl2::invoke(proj, *m))) {
-					*++first = iter_move(m);
+	struct __unique_fn : private __niebloid {
+		template<Permutable I, Sentinel<I> S, class Proj = identity,
+			IndirectRelation<projected<I, Proj>> Comp = equal_to>
+		constexpr I
+		operator()(I first, S last, Comp comp = {}, Proj proj = {}) const {
+			first = adjacent_find(std::move(first), last, __stl2::ref(comp),
+				__stl2::ref(proj));
+			if (first != last) {
+				for (auto m = next(first, 2); m != last; ++m) {
+					if (!__stl2::invoke(comp, __stl2::invoke(proj, *first),
+							__stl2::invoke(proj, *m)))
+					{
+						*++first = iter_move(m);
+					}
 				}
+				++first;
 			}
-			++first;
+			return first;
 		}
-		return first;
-	}
 
-	template<ForwardRange Rng, class R = equal_to, class Proj = identity>
-	requires
-		Permutable<iterator_t<Rng>> &&
-		IndirectRelation<
-			__f<R>, projected<iterator_t<Rng>, Proj>>
-	safe_iterator_t<Rng>
-	unique(Rng&& rng, R comp = {}, Proj proj = {})
-	{
-		return __stl2::unique(begin(rng), end(rng),
-			__stl2::ref(comp), __stl2::ref(proj));
-	}
+		template<ForwardRange R, class Proj = identity,
+			IndirectRelation<projected<iterator_t<R>, Proj>> Comp = equal_to>
+		requires Permutable<iterator_t<R>>
+		constexpr safe_iterator_t<R>
+		operator()(R&& r, Comp comp = {}, Proj proj = {}) const {
+			return (*this)(begin(r), end(r), __stl2::ref(comp),
+				__stl2::ref(proj));
+		}
+	};
+
+	inline constexpr __unique_fn unique {};
 } STL2_CLOSE_NAMESPACE
 
 #endif
