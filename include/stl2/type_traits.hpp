1a9a550d0c0beed9e74ce3f82986b315a3a91523
Casey Carter <Casey@Carter.net>
2019-01-09 09:34:55 -0800

    common_type: Sync to WD
    Fixes #135.

--- include/stl2/type_traits.hpp
+++ include/stl2/type_traits.hpp
@@ -19,8 +19,73 @@
 #include <stl2/detail/concepts/core.hpp>
 
 STL2_OPEN_NAMESPACE {
-	///////////////////////////////////////////////////////////////////////////
-	// common_type
+	////////////////////////////////////////////////////////////////////////////
+	// common_type [meta.trans.other]
+	//
+
+	// Modified CREF from [meta.trans.other]/2.1 that doesn't work with void.
+	template<class T>
+	using __cref_nonvoid = const std::remove_reference_t<T>&;
+
+	// COND_RES [meta.trans.other]/2.4
+	template<class T, class U>
+	using __cond_res =
+		decltype(false ? std::declval<T(&)()>()() : std::declval<U(&)()>()());
+
+	template<class...>
+	struct common_type {};
+
+	template<class... Ts>
+	using common_type_t = meta::_t<common_type<Ts...>>;
+
+	// [meta.trans.other]/3.2
+	template<class T>
+	struct common_type<T> : common_type<std::decay_t<T>, std::decay_t<T>> {};
+
+	// [meta.trans.other]/3.3: "Otherwise, there shall be no member type."
+	template<class, class> // NB: The arguments have been decayed by the time they get here.
+	struct __common_type3 {};
+
+	// [meta.trans.other]/3.3.4
+	// I'm 99% certain that [meta.trans.other]/3.3.4 never produces a useful
+	// common type when T or U is void - proper cases for void would be handled
+	// earlier in 3.3.3 - so __cref_nonvoid is fine here.
+	template<class T, class U> // NB: Ditto decayed.
+	requires requires { typename __cond_res<__cref_nonvoid<T>, __cref_nonvoid<U>>; }
+	struct __common_type3<T, U> {
+		using type = std::decay_t<__cond_res<__cref_nonvoid<T>, __cref_nonvoid<U>>>;
+	};
+
+	template<class T, class U> // NB: Ditto decayed.
+	struct __common_type2 : __common_type3<T, U> {};
+
+	// Note: std::common_type handles [meta.trans.other]/3.3.3
+	template<class T, class U> // NB: Ditto decayed.
+	requires requires { typename std::common_type_t<T, U>; }
+	struct __common_type2<T, U> {
+		using type = std::common_type_t<T, U>;
+	};
+
+	template<class T, class U>
+	struct common_type<T, U> : __common_type2<T, U> {};
+
+	template<class T, class U>
+	requires (!_Decayed<T> || !_Decayed<U>)
+	struct common_type<T, U> : common_type<std::decay_t<T>, std::decay_t<U>> {};
+
+	template<class T, class U, class V, class...  W>
+	requires requires { typename common_type_t<std::decay_t<T>, std::decay_t<U>>; }
+	struct common_type<T, U, V, W...>
+	: common_type<common_type_t<std::decay_t<T>, std::decay_t<U>>, V, W...> {};
+
+	////////////////////////////////////////////////////////////////////////////
+	// basic_common_reference [meta.trans.other]
+	//
+	template<class, class, template<class> class, template<class> class>
+	struct basic_common_reference {};
+
+	////////////////////////////////////////////////////////////////////////////
+	// common_reference [meta.trans.other]
 	//
 	template<class T>
 	struct __unary {
@@ -28,15 +93,9 @@ STL2_OPEN_NAMESPACE {
 		using apply = meta::invoke<T, U>;
 	};
 
+	// CREF [meta.trans.other]/2.1
 	template<class T>
-	using __ref = std::add_lvalue_reference_t<T>;
-
-	template<class T, class X = std::remove_reference_t<T>>
-	using __cref = __ref<std::add_const_t<X>>;
-
-	template<class T, class U>
-	using __cond =
-		decltype(true ? std::declval<T(&)()>()() : std::declval<U(&)()>()());
+	using __cref = std::add_lvalue_reference_t<const std::remove_reference_t<T>>;
 
 	template<class From, class To>
 	struct __copy_cv_ : meta::id<To> {};
@@ -54,11 +113,11 @@ STL2_OPEN_NAMESPACE {
 	template<class T, class U>
 	using __builtin_common_t = meta::_t<__builtin_common<T, U>>;
 	template<class T, class U>
-	requires meta::Valid<__cond, __cref<T>, __cref<U>>
-	struct __builtin_common<T, U> : std::decay<__cond<__cref<T>, __cref<U>>> {};
+	requires meta::Valid<__cond_res, __cref<T>, __cref<U>>
+	struct __builtin_common<T, U> : std::decay<__cond_res<__cref<T>, __cref<U>>> {};
 
 	template<class T, class U, class R = __builtin_common_t<T &, U &>>
-	using __rref_res = std::conditional_t<meta::_v<std::is_reference<R>>,
+	using __rref_res = std::conditional_t<std::is_reference_v<R>,
 		std::remove_reference_t<R> &&, R>;
 
 	template<class T, class U>
@@ -69,7 +128,7 @@ STL2_OPEN_NAMESPACE {
 	struct __builtin_common<T &&, U &&> : meta::id<__rref_res<T, U>> {};
 
 	template<class T, class U>
-	using __lref_res = __cond<__copy_cv<T, U> &, __copy_cv<U, T> &>;
+	using __lref_res = __cond_res<__copy_cv<T, U> &, __copy_cv<U, T> &>;
 
 	template<class T, class U>
 	struct __builtin_common<T &, U &> : meta::defer<__lref_res, T, U> {};
@@ -82,37 +141,6 @@ STL2_OPEN_NAMESPACE {
 	template<class T, class U>
 	struct __builtin_common<T &&, U &> : __builtin_common<U &, T &&> {};
 
-	// common_type
-	template<class...>
-	struct common_type {};
-
-	template<class... Ts>
-	using common_type_t = meta::_t<common_type<Ts...>>;
-
-	template<class T>
-	struct common_type<T> : std::decay<T> {};
-
-	template<class T, class U>
-	struct __common_type2 : common_type<std::decay_t<T>, std::decay_t<U>> {};
-
-	template<_Decayed T, _Decayed U>
-	struct __common_type2<T, U> : __builtin_common<T, U> {};
-
-	template<_Decayed T, _Decayed U>
-		requires requires { typename std::common_type_t<T, U>; }
-	struct __common_type2<T, U> : std::common_type<T, U> {};
-
-	template<class T, class U>
-	struct common_type<T, U> : __common_type2<T, U> {};
-
-	template<class T, class U, class V, class...  W>
-	requires meta::Valid<common_type_t, T, U>
-	struct common_type<T, U, V, W...>
-	: common_type<common_type_t<T, U>, V, W...> {};
-
-	///////////////////////////////////////////////////////////////////////////
-	// common_reference machinery
-	//
 	namespace __qual {
 		using __rref = meta::quote<std::add_rvalue_reference_t>;
 		using __lref = meta::quote<std::add_lvalue_reference_t>;
@@ -130,16 +158,12 @@ STL2_OPEN_NAMESPACE {
 		struct __xref<const volatile T> : meta::id<meta::quote<std::add_cv_t>> {};
 	}
 
-	template<class T, class U, template<class> class TQual, template<class> class UQual>
-	struct basic_common_reference {};
-
 	template<class T, class U>
 	using __basic_common_reference =
 		basic_common_reference<__uncvref<T>, __uncvref<U>,
 			__unary<meta::_t<__qual::__xref<T>>>::template apply,
 			__unary<meta::_t<__qual::__xref<U>>>::template apply>;
 
-	// common_reference
 	template<class...>
 	struct common_reference {};
 
@@ -153,8 +177,8 @@ STL2_OPEN_NAMESPACE {
 	struct __common_reference2_2_ : common_type<T, U> {};
 
 	template<class T, class U>
-	requires requires { typename __cond<T, U>; }
-	struct __common_reference2_2_<T, U> : meta::id<__cond<T, U>> {};
+	requires requires { typename __cond_res<T, U>; }
+	struct __common_reference2_2_<T, U> : meta::id<__cond_res<T, U>> {};
 
 	template<class T, class U>
 	struct __common_reference2_1_ : __common_reference2_2_<T, U> {};
