1efbc45f75459d93c30b04edfb53d700e446d075
Casey Carter <Casey@Carter.net>
2019-01-09 11:28:02 -0800

    common_reference: sync to WD
    ...and refactor to instantiate fewer classes / use less inheritance.

--- include/stl2/type_traits.hpp
+++ include/stl2/type_traits.hpp
@@ -87,82 +87,110 @@ STL2_OPEN_NAMESPACE {
 	////////////////////////////////////////////////////////////////////////////
 	// common_reference [meta.trans.other]
 	//
-	template<class T>
-	struct __unary {
-		template<class U>
-		using apply = meta::invoke<T, U>;
+	template<class From>
+	struct __copy_cv_ {
+		static_assert(!std::is_reference_v<From>);
+		template<class To> using invoke = To;
 	};
+	template<class From>
+	struct __copy_cv_<const From> {
+		template<class To> using invoke = const To;
+	};
+	template<class From>
+	struct __copy_cv_<volatile From> {
+		template<class To> using invoke = volatile To;
+	};
+	template<class From>
+	struct __copy_cv_<const volatile From> {
+		template<class To> using invoke = const volatile To;
+	};
+	template<class From, class To>
+	using __copy_cv = meta::invoke<__copy_cv_<From>, To>;
 
 	// CREF [meta.trans.other]/2.1
 	template<class T>
 	using __cref = std::add_lvalue_reference_t<const std::remove_reference_t<T>>;
 
-	template<class From, class To>
-	struct __copy_cv_ : meta::id<To> {};
-	template<class From, class To>
-	struct __copy_cv_<From const, To> : std::add_const<To> {};
-	template<class From, class To>
-	struct __copy_cv_<From volatile, To> : std::add_volatile<To> {};
-	template<class From, class To>
-	struct __copy_cv_<From const volatile, To> : std::add_cv<To> {};
-	template<class From, class To>
-	using __copy_cv = meta::_t<__copy_cv_<From, To>>;
+	// COMMON_REF [meta.trans.other]/2
+	template<class T, class U>
+	struct __common_ref_ {
+		static_assert(std::is_reference_v<T>);
+		static_assert(std::is_reference_v<U>);
+	};
 
 	template<class T, class U>
-	struct __builtin_common {};
-	template<class T, class U>
-	using __builtin_common_t = meta::_t<__builtin_common<T, U>>;
-	template<class T, class U>
-	requires meta::Valid<__cond_res, __cref<T>, __cref<U>>
-	struct __builtin_common<T, U> : std::decay<__cond_res<__cref<T>, __cref<U>>> {};
+	using __common_ref = meta::_t<__common_ref_<T, U>>;
 
-	template<class T, class U, class R = __builtin_common_t<T &, U &>>
-	using __rref_res = std::conditional_t<std::is_reference_v<R>,
-		std::remove_reference_t<R> &&, R>;
-
-	template<class T, class U>
-	requires
-		meta::Valid<__builtin_common_t, T &, U &> &&
-		ConvertibleTo<T &&, __rref_res<T, U>> &&
-		ConvertibleTo<U &&, __rref_res<T, U>>
-	struct __builtin_common<T &&, U &&> : meta::id<__rref_res<T, U>> {};
-
+	// [meta.trans.other]/2.5
 	template<class T, class U>
 	using __lref_res = __cond_res<__copy_cv<T, U> &, __copy_cv<U, T> &>;
 
 	template<class T, class U>
-	struct __builtin_common<T &, U &> : meta::defer<__lref_res, T, U> {};
+	requires requires { typename __lref_res<T, U>; } &&
+		std::is_reference_v<__lref_res<T, U>>
+	struct __common_ref_<T&, U&> {
+		using type = __lref_res<T, U>;
+	};
+
+	// [meta.trans.other]/2.6
+	template<class T, class U, class R = __common_ref<T&, U&>>
+	using __rref_res = std::remove_reference_t<R>&&;
 
 	template<class T, class U>
-	requires
-		meta::Valid<__builtin_common_t, T &, U const &> &&
-		ConvertibleTo<U &&, __builtin_common_t<T &, U const &>>
-	struct __builtin_common<T &, U &&> : __builtin_common<T &, U const &> {};
-	template<class T, class U>
-	struct __builtin_common<T &&, U &> : __builtin_common<U &, T &&> {};
+	requires meta::Valid<__common_ref, T&, U&> &&
+		ConvertibleTo<T&&, __rref_res<T, U>> &&
+		ConvertibleTo<U&&, __rref_res<T, U>>
+	struct __common_ref_<T&&, U&&> {
+		using type = __rref_res<T, U>;
+	};
 
-	namespace __qual {
-		using __rref = meta::quote<std::add_rvalue_reference_t>;
-		using __lref = meta::quote<std::add_lvalue_reference_t>;
-		template<class>
-		struct __xref : meta::id<meta::compose<meta::quote<meta::_t>, meta::quote<meta::id>>> {};
-		template<class T>
-		struct __xref<T&> : meta::id<meta::compose<__lref, meta::_t<__xref<T>>>> {};
-		template<class T>
-		struct __xref<T&&> : meta::id<meta::compose<__rref, meta::_t<__xref<T>>>> {};
-		template<class T>
-		struct __xref<const T> : meta::id<meta::quote<std::add_const_t>> {};
-		template<class T>
-		struct __xref<volatile T> : meta::id<meta::quote<std::add_volatile_t>> {};
-		template<class T>
-		struct __xref<const volatile T> : meta::id<meta::quote<std::add_cv_t>> {};
-	}
+	// [meta.trans.other]/2.7
+	template<class T, class U>
+	requires requires { typename __common_ref<const T&, U&>; } &&
+		ConvertibleTo<T&&, __common_ref<const T&, U&>>
+	struct __common_ref_<T&&, U&> {
+		using type = __common_ref<const T&, U&>;
+	};
+
+	// [meta.trans.other]/2.8
+	template<class T, class U>
+	requires requires { typename __common_ref<T&, const U&>; } &&
+		ConvertibleTo<U&&, __common_ref<T&, const U&>>
+	struct __common_ref_<T&, U&&> {
+		using type = __common_ref<T&, const U&>;
+	};
+
+	template<class>
+	struct __xref {
+		template<class U> using invoke = U;
+	};
+	template<class T>
+	struct __xref<const T> {
+		template<class U> using invoke = const U;
+	};
+	template<class T>
+	struct __xref<volatile T> {
+		template<class U> using invoke = volatile U;
+	};
+	template<class T>
+	struct __xref<const volatile T> {
+		template<class U> using invoke = const volatile U;
+	};
+	template<class T>
+	struct __xref<T&> {
+		template<class U> using invoke =
+			std::add_lvalue_reference_t<meta::invoke<__xref<T>, U>>;
+	};
+	template<class T>
+	struct __xref<T&&> {
+		template<class U> using invoke =
+			std::add_rvalue_reference_t<meta::invoke<__xref<T>, U>>;
+	};
 
 	template<class T, class U>
-	using __basic_common_reference =
-		basic_common_reference<__uncvref<T>, __uncvref<U>,
-			__unary<meta::_t<__qual::__xref<T>>>::template apply,
-			__unary<meta::_t<__qual::__xref<U>>>::template apply>;
+	using __basic_common_reference_t =
+		meta::_t<basic_common_reference<__uncvref<T>, __uncvref<U>,
+			__xref<T>::template invoke, __xref<U>::template invoke>>;
 
 	template<class...>
 	struct common_reference {};
@@ -170,37 +198,47 @@ STL2_OPEN_NAMESPACE {
 	template<class... Ts>
 	using common_reference_t = meta::_t<common_reference<Ts...>>;
 
+	// [meta.trans.other]/5.2
 	template<class T>
-	struct common_reference<T> : meta::id<T> {};
+	struct common_reference<T> {
+		using type = T;
+	};
 
+	// [meta.trans.other]/5.3.4
 	template<class T, class U>
-	struct __common_reference2_2_ : common_type<T, U> {};
+	struct __common_reference3 : common_type<T, U> {};
 
+	// [meta.trans.other]/5.3.3
 	template<class T, class U>
 	requires requires { typename __cond_res<T, U>; }
-	struct __common_reference2_2_<T, U> : meta::id<__cond_res<T, U>> {};
+	struct __common_reference3<T, U> {
+		using type = __cond_res<T, U>;
+	};
 
 	template<class T, class U>
-	struct __common_reference2_1_ : __common_reference2_2_<T, U> {};
+	struct __common_reference2 : __common_reference3<T, U> {};
 
+	// [meta.trans.other]/5.3.2
 	template<class T, class U>
-	requires meta::Valid<meta::_t, __basic_common_reference<T, U>>
-	struct __common_reference2_1_<T, U> : __basic_common_reference<T, U> {};
-
-	template<class T, class U>
-	struct __common_reference2 : __common_reference2_1_<T, U> {};
-
-	template<class T, class U>
-	requires std::is_reference_v<T> && std::is_reference_v<U> &&
-		meta::Valid<__builtin_common_t, T, U> &&
-		std::is_reference_v<__builtin_common_t<T, U>>
-	struct __common_reference2<T, U> : __builtin_common<T, U> {};
+	requires requires { typename __basic_common_reference_t<T, U>; }
+	struct __common_reference2<T, U> {
+		using type = __basic_common_reference_t<T, U>;
+	};
 
 	template<class T, class U>
 	struct common_reference<T, U> : __common_reference2<T, U> {};
 
+	// [meta.trans.other]/5.3.1
+	template<class T, class U>
+	requires std::is_reference_v<T> && std::is_reference_v<U> &&
+		requires { typename __common_ref<T, U>; }
+	struct common_reference<T, U> {
+		using type = __common_ref<T, U>;
+	};
+
+	// [meta.trans.other]/5.4
 	template<class T, class U, class V, class... W>
-	requires meta::Valid<common_reference_t, T, U>
+	requires requires { typename common_reference_t<T, U>; }
 	struct common_reference<T, U, V, W...>
 	: common_reference<common_reference_t<T, U>, V, W...> {};
 
