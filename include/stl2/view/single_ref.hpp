ec880d73d7e10e183a751a12ada522530f6053b1
Dalton M. Woodard <dalton@daltonmwoodard.com>
2019-01-26 15:34:46 -0800

    Indent contents of namespace ext
    Signed-off-by: Dalton M. Woodard <dalton@daltonmwoodard.com>

--- include/stl2/view/single_ref.hpp
+++ include/stl2/view/single_ref.hpp
@@ -23,43 +23,43 @@
 #include <stl2/view/view_interface.hpp>
 
 STL2_OPEN_NAMESPACE {
-namespace ext {
-	template<class T>
-	requires std::is_object_v<T>
-	class single_ref_view : public view_interface<single_ref_view<T>> {
-	private:
-		T* ref_ = nullptr;
+	namespace ext {
+		template<class T>
+		requires std::is_object_v<T>
+		class single_ref_view : public view_interface<single_ref_view<T>> {
+		private:
+			T* ref_ = nullptr;
 
-		// following the same constraints as ref_view
-		static void fun(T&) noexcept;
-		static void fun(T&&) = delete;
-	public:
-		constexpr single_ref_view() = default;
+			// following the same constraints as ref_view
+			static void fun(T&) noexcept;
+			static void fun(T&&) = delete;
+		public:
+			constexpr single_ref_view() = default;
 
 #if STL2_WORKAROUND_CLANGC_42
-		template<class U>
-		requires _NotSameAs<U, single_ref_view> &&
-			requires(U&& u) { fun(static_cast<U&&>(u)); }
+			template<class U>
+			requires _NotSameAs<U, single_ref_view> &&
+				requires(U&& u) { fun(static_cast<U&&>(u)); }
 #else
-		template<_NotSameAs<single_ref_view> U>
-		requires requires(U&& u) { fun(static_cast<U&&>(u)); }
+			template<_NotSameAs<single_ref_view> U>
+			requires requires(U&& u) { fun(static_cast<U&&>(u)); }
 #endif
-		constexpr explicit single_ref_view(U&& u)
-		noexcept(noexcept(fun(static_cast<U&&>(u)))) // strengthened
-		: ref_{std::addressof(static_cast<T&>(static_cast<U&&>(u)))} {}
+			constexpr explicit single_ref_view(U&& u)
+			noexcept(noexcept(fun(static_cast<U&&>(u)))) // strengthened
+			: ref_{std::addressof(static_cast<T&>(static_cast<U&&>(u)))} {}
 
-		constexpr T* begin() noexcept { return data(); }
-		constexpr const T* begin() const noexcept { return data(); }
-		constexpr T* end() noexcept { return data() + 1; }
-		constexpr const T* end() const noexcept { return data() + 1; }
-		constexpr static std::ptrdiff_t size() noexcept { return 1; }
-		constexpr T* data() noexcept { return ref_; }
-		constexpr const T* data() const noexcept { return ref_; }
-	};
+			constexpr T* begin() noexcept { return data(); }
+			constexpr const T* begin() const noexcept { return data(); }
+			constexpr T* end() noexcept { return data() + 1; }
+			constexpr const T* end() const noexcept { return data() + 1; }
+			constexpr static std::ptrdiff_t size() noexcept { return 1; }
+			constexpr T* data() noexcept { return ref_; }
+			constexpr const T* data() const noexcept { return ref_; }
+		};
 
-	template <class T>
-	single_ref_view(T&) -> single_ref_view<T>;
-}
+		template <class T>
+		single_ref_view(T&) -> single_ref_view<T>;
+	} // namespace ext
 
 	namespace view::ext {
 		struct __single_ref_fn : detail::__pipeable<__single_ref_fn> {
