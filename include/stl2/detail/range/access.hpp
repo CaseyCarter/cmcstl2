0563085eb62c8b43e82ffe02f19100ec29229181
Casey Carter <Casey@Carter.net>
2019-08-26 20:03:52 -0700

    Don't instantiate member or non-member size in ranges::size when disable_sized_range

--- include/stl2/detail/range/access.hpp
+++ include/stl2/detail/range/access.hpp
@@ -412,18 +412,22 @@ STL2_OPEN_NAMESPACE {
 			template<class R, class T = std::remove_reference_t<R>,
 				class U = std::remove_cv_t<T>>
 			requires std::is_array_v<T>
-				|| (!disable_sized_range<U>
-					&& (has_member<T> || has_non_member<T>))
+				|| (!disable_sized_range<U> && (has_member<T> || has_non_member<T>))
 				|| has_difference<T>
 			constexpr auto operator()(R&& r) const noexcept(nothrow<T>) {
 				if constexpr (std::is_array_v<T>)
 					return std::extent_v<T>;
-				else if constexpr (!disable_sized_range<U> && has_member<T>)
-					return r.size();
-				else if constexpr (!disable_sized_range<U> && has_non_member<T>)
-					return size(r);
-				else // has_difference<T>
+				else if constexpr (disable_sized_range<U>) {
+					static_assert(has_difference<T>);
 					return end(r) - begin(r);
+				} else if constexpr (has_member<T>)
+					return r.size();
+				else if constexpr (has_non_member<T>)
+					return size(r);
+				else {
+					static_assert(has_difference<T>);
+					return end(r) - begin(r);
+				}
 			}
 		};
 	}
