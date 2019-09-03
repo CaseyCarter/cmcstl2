fe29a2aa5b01e628e21d33087f6724a223400e74
Casey Carter <Casey@Carter.net>
2019-09-02 18:42:13 -0700

    any_iterator cleanup
    * Style
    * avoid `aligned_storage`
    * don't use small storage when underaligned

--- include/stl2/detail/iterator/any_iterator.hpp
+++ include/stl2/detail/iterator/any_iterator.hpp
@@ -27,83 +27,85 @@ STL2_OPEN_NAMESPACE {
 		enum class op { copy, move, nuke, bump, comp, rval };
 
 		struct counted {
-			std::atomic<long> cnt{ 1 };
+			std::atomic<long> cnt{1};
 		};
-		template<input_iterator I> struct shared_iterator : counted {
-			shared_iterator(I i) : it(std::move(i)) {}
+		template<input_iterator I>
+		struct shared_iterator : counted {
+			explicit shared_iterator(I i)
+			noexcept(std::is_nothrow_move_constructible_v<I>)
+			: it(std::move(i)) {}
+
 			I it;
 		};
 
 		union blob {
 			counted* big;
-			std::aligned_storage_t<2 * sizeof(void *)> tiny;
+			alignas(std::max_align_t) unsigned char tiny[2 * sizeof(void*)];
 		};
 
 		template<class It>
-		using is_small =
-			std::integral_constant<bool, (sizeof(It) <= sizeof(blob::tiny))>;
+		using is_small = std::integral_constant<bool,
+			(sizeof(It) <= sizeof(blob::tiny) && alignof(It) <= alignof(std::max_align_t))>;
 		using small_tag = std::true_type;
 		using big_tag = std::false_type;
 
 		template<class RValueReference>
-		using iter_move_fn = RValueReference (*)(blob const &);
+		using iter_move_fn = RValueReference (*)(const blob&);
 
 		template<class RValueReference>
-		inline iter_move_fn<RValueReference> uninit_noop(op, blob *, blob *) {
+		inline iter_move_fn<RValueReference> uninit_noop(op, blob*, blob*) {
 			return nullptr;
 		}
 
 		template<class Reference>
-		[[noreturn]] inline Reference uninit_deref(blob const &) {
+		[[noreturn]] inline Reference uninit_deref(const blob&) {
 			std::terminate();
 		}
 
 		template<class Reference, input_iterator I>
-		Reference deref_small(blob const &src) {
-			return **static_cast<I const *>(static_cast<void const *>(&src.tiny));
+		Reference deref_small(const blob& src) {
+			return *reinterpret_cast<const I&>(src.tiny);
 		}
 
 		template<class Reference, input_iterator I>
-		Reference deref_big(blob const &src) {
-			return *static_cast<shared_iterator<I> const *>(src.big)->it;
+		Reference deref_big(const blob& src) {
+			return *static_cast<const shared_iterator<I>*>(src.big)->it;
 		}
 
 		template<class I, class J>
-		constexpr bool iter_equal(I const &, J const &) {
-			return true;
-		}
-
-		template<class I, sentinel_for<I> J>
-		constexpr bool iter_equal(I const &i, J const &j) {
-			return i == j;
+		constexpr bool iter_equal(const I& i, const J& j) {
+			if constexpr (sentinel_for<J, I>) {
+				return i == j;
+			} else {
+				return true;
+			}
 		}
 
 		template<class RValueReference, input_iterator I>
-		iter_move_fn<RValueReference> exec_small(op o, blob *src, blob *dst) {
+		iter_move_fn<RValueReference> exec_small(op o, blob* src, blob* dst) {
 			switch (o) {
 			case op::copy:
-				::new (static_cast<void *>(&dst->tiny))
-					I(*static_cast<I const *>(static_cast<void *>(&src->tiny)));
+				::new (static_cast<void*>(&dst->tiny))
+					I(reinterpret_cast<const I&>(src->tiny));
 				break;
 			case op::move:
-				::new (static_cast<void *>(&dst->tiny))
-					I(std::move(*static_cast<I *>(static_cast<void *>(&src->tiny))));
-				// fallthrough
+				::new (static_cast<void*>(&dst->tiny))
+					I(reinterpret_cast<I&&>(src->tiny));
+				[[fallthrough]];
 			case op::nuke:
-				static_cast<I *>(static_cast<void *>(&src->tiny))->~I();
+				reinterpret_cast<I&>(src->tiny).~I();
 				break;
 			case op::bump:
-				++*static_cast<I *>(static_cast<void *>(&src->tiny));
+				++reinterpret_cast<I&>(src->tiny);
 				break;
 			case op::comp:
 				if (__any_iterator::iter_equal(
-					*static_cast<I const *>(static_cast<void *>(&src->tiny)),
-					*static_cast<I const *>(static_cast<void *>(&dst->tiny)))) {
-					// fallthrough
+					reinterpret_cast<const I&>(src->tiny),
+					reinterpret_cast<const I&>(dst->tiny))) {
+					[[fallthrough]];
 			case op::rval:
-					return +[](blob const &src) -> RValueReference {
-						return iter_move(
-							*static_cast<I const *>(static_cast<void const *>(&src.tiny)));
+					return +[](const blob& src) -> RValueReference {
+						return iter_move(reinterpret_cast<const I&>(src.tiny));
 					};
 				}
 			}
@@ -111,30 +113,32 @@ STL2_OPEN_NAMESPACE {
 		}
 
 		template<class RValueReference, input_iterator I>
-		iter_move_fn<RValueReference> exec_big(op o, blob *src, blob *dst) {
+		iter_move_fn<RValueReference> exec_big(op o, blob* src, blob* dst) {
 			switch (o) {
 			case op::copy:
-				++(dst->big = src->big)->cnt;
+				dst->big = src->big;
+				++src->big->cnt;
 				break;
 			case op::move:
 				dst->big = __stl2::exchange(src->big, nullptr);
 				break;
 			case op::nuke:
-				if (0 == --src->big->cnt)
+				if (--src->big->cnt == 0) {
 					delete static_cast<shared_iterator<I>*>(src->big);
+				}
 				break;
 			case op::bump:
-				++static_cast<shared_iterator<I> *>(src->big)->it;
+				++static_cast<shared_iterator<I>*>(src->big)->it;
 				break;
 			case op::comp:
 				if (__any_iterator::iter_equal(
-					static_cast<shared_iterator<I> const *>(src->big)->it,
-					static_cast<shared_iterator<I> const *>(dst->big)->it)) {
-					// fallthrough
+					static_cast<const shared_iterator<I>*>(src->big)->it,
+					static_cast<const shared_iterator<I>*>(dst->big)->it)) {
+					[[fallthrough]];
 			case op::rval:
-					return +[](blob const &src) -> RValueReference {
+					return +[](const blob& src) -> RValueReference {
 						return iter_move(
-							static_cast<shared_iterator<I> const *>(src.big)->it);
+							static_cast<const shared_iterator<I>*>(src.big)->it);
 					};
 				}
 			}
@@ -144,18 +148,19 @@ STL2_OPEN_NAMESPACE {
 		template<class Reference, class ValueType, class RValueReference>
 		struct cursor {
 		private:
-			blob data_ = { nullptr };
-			Reference (*deref_)(blob const &) =
-				&uninit_deref<Reference>;
-			iter_move_fn<RValueReference> (*exec_)(op, blob *, blob *) =
+			blob data_{nullptr};
+			Reference (*deref_)(const blob&) = &uninit_deref<Reference>;
+			iter_move_fn<RValueReference> (*exec_)(op, blob*, blob*) =
 				&uninit_noop<RValueReference>;
 
-			template<input_iterator I> cursor(I i, small_tag) {
+			template<input_iterator I>
+			cursor(I i, small_tag) {
 				::new (static_cast<void *>(&data_.tiny)) I(std::move(i));
 				deref_ = &deref_small<Reference, I>;
 				exec_ = &exec_small<RValueReference, I>;
 			}
-			template<input_iterator I> cursor(I i, big_tag) {
+			template<input_iterator I>
+			cursor(I i, big_tag) {
 				data_.big = new shared_iterator<I>(std::move(i));
 				deref_ = &deref_big<Reference, I>;
 				exec_ = &exec_big<RValueReference, I>;
@@ -165,13 +170,13 @@ STL2_OPEN_NAMESPACE {
 				deref_ = &uninit_deref<Reference>;
 				exec_ = &uninit_noop<RValueReference>;
 			}
-			void copy_from(cursor const &that) {
+			void copy_from(const cursor& that) {
 				// Pre: *this is empty
-				that.exec_(op::copy, const_cast<blob *>(&that.data_), &data_);
+				that.exec_(op::copy, const_cast<blob*>(&that.data_), &data_);
 				deref_ = that.deref_;
 				exec_ = that.exec_;
 			}
-			void move_from(cursor &that) {
+			void move_from(cursor& that) {
 				// Pre: *this is empty
 				that.exec_(op::move, &that.data_, &data_);
 				__stl2::swap(deref_, that.deref_);
@@ -198,23 +203,22 @@ STL2_OPEN_NAMESPACE {
 			};
 
 			cursor() = default;
-			cursor(cursor &&that) {
+			cursor(cursor&& that) {
 				move_from(that);
 			}
-			cursor(cursor const &that) {
+			cursor(const cursor& that) {
 				copy_from(that);
 			}
-			template<input_iterator I> cursor(I i)
-			: cursor{std::move(i), is_small<I>{}}
-			{}
-			cursor &operator=(cursor &&that) {
+			template<input_iterator I>
+			cursor(I i) : cursor{std::move(i), is_small<I>{}} {}
+			cursor& operator=(cursor&& that) {
 				if (&that != this) {
 					reset();
 					move_from(that);
 				}
 				return *this;
 			}
-			cursor &operator=(cursor const &that) {
+			cursor& operator=(const cursor& that) {
 				if (&that != this) {
 					reset();
 					copy_from(that);
@@ -227,9 +231,9 @@ STL2_OPEN_NAMESPACE {
 			Reference read() const {
 				return deref_(data_);
 			}
-			bool equal(cursor const &that) const {
-				return exec_(op::comp, const_cast<blob *>(&data_),
-					const_cast<blob *>(&that.data_)) != nullptr;
+			bool equal(const cursor& that) const {
+				return exec_(op::comp, const_cast<blob*>(&data_),
+					const_cast<blob*>(&that.data_)) != nullptr;
 			}
 			void next() {
 				exec_(op::bump, &data_, nullptr);
